8b7e077bbe9c91e56a4a4f6ce80d6c75e949fe1d
jivern <jivern@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-08-16 15:17:19 +0000

    Moved remotely
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@33019 fb370eea-3af6-4597-97f7-f7400a59c12a

--- project_management/trunk/test_files/ClearDB.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM resource_info WHERE resource_id IN (1, 2);
-DELETE FROM resource WHERE resource_id IN (1, 2);
-DELETE FROM project_info WHERE project_info_type_id IN (1, 2, 3);
-DELETE FROM project WHERE project_id IN (1, 2, 3, 4, 5);
-DELETE FROM project_info_type_lu WHERE project_info_type_id IN (1, 2, 3);
-DELETE FROM project_category_lu WHERE project_category_id IN (1, 2, 3, 4);
-DELETE FROM project_type_lu WHERE project_type_id IN (1, 2, 3);
