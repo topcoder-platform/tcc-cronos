c5322136f299450b9965af7eda094bff0beb4e64
WishingBone <WishingBone@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-08-17 09:58:10 +0000

    Modified exceptions
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@33151 fb370eea-3af6-4597-97f7-f7400a59c12a

--- deliverable_management/trunk/src/java/main/com/topcoder/management/deliverable/persistence/DeliverablePersistenceException.java
+++ deliverable_management/trunk/src/java/main/com/topcoder/management/deliverable/persistence/DeliverablePersistenceException.java
@@ -3,25 +3,29 @@
  */
 package com.topcoder.management.deliverable.persistence;
 
-import com.topcoder.util.errorhandling.BaseException;
-
 /**
  * <p>
- * The DeliverablePersistenceException indicates that there was an error accessing or updating a
- * persisted resource store. This exception is used to wrap the internal error that occurs when
- * accessing the persistence store. For example, in the SqlDeliverablePersistence implementation it
- * is used to wrap SqlExceptions.
+ * The DeliverablePersistenceException indicates that there was an error
+ * accessing or updating a persisted resource store. This exception is used to
+ * wrap the internal error that occurs when accessing the persistence store. For
+ * example, in the SqlDeliverablePersistence implementation it is used to wrap
+ * SqlExceptions.
  * </p>
- *
- * @author aubergineanode, singlewood
+ * <p>
+ * This exception is initially thrown in DeliverablePersistence implementations
+ * and from there passes through DeliverableManager implementations and back to
+ * the caller. It is also thrown directly by some DeliverableManager
+ * implementations.
+ * </p>
+ * @author aubergineanode, TCSDEVELOPER
  * @version 1.0
  */
-public class DeliverablePersistenceException extends BaseException {
+public class DeliverablePersistenceException extends PersistenceException {
 
     /**
-     * Creates a new DeliverablePersistenceException.
-     *
-     * @param message Explanation of error, can be null
+     * reates a new DeliverablePersistenceException.
+     * @param message
+     *            Explanation of error, can be null
      */
     public DeliverablePersistenceException(String message) {
         super(message);
@@ -29,9 +33,10 @@ public class DeliverablePersistenceException extends BaseException {
 
     /**
      * Creates a new DeliverablePersistenceException.
-     *
-     * @param message Explanation of error, can be null
-     * @param cause Underlying cause of error, can be null
+     * @param message
+     *            Explanation of error, can be null
+     * @param cause
+     *            Underlying cause of error, can be null
      */
     public DeliverablePersistenceException(String message, Exception cause) {
         super(message, cause);
--- deliverable_management/trunk/src/java/main/com/topcoder/management/deliverable/persistence/UploadPersistenceException.java
+++ deliverable_management/trunk/src/java/main/com/topcoder/management/deliverable/persistence/UploadPersistenceException.java
@@ -3,30 +3,27 @@
  */
 package com.topcoder.management.deliverable.persistence;
 
-import com.topcoder.util.errorhandling.BaseException;
-
 /**
  * <p>
- * The UploadPersistenceException indicates that there was an error accessing or updating a
- * persisted resource store. This exception is used to wrap the internal error that occurs when
- * accessing the persistence store. For example, in the SqlUploadPersistence implementation it is
- * used to wrap SQLExceptions.
+ * The UploadPersistenceException indicates that there was an error accessing or
+ * updating a persisted resource store. This exception is used to wrap the
+ * internal error that occurs when accessing the persistence store. For example,
+ * in the SqlUploadPersistence implementation it is used to wrap SqlExceptions.
  * </p>
  * <p>
- * This exception is initially thrown in UploadPersistence implementations and from there passes
- * through UploadManager implementations and back to the caller. It is also thrown directly by some
- * UploadManager implementations.
+ * This exception is initially thrown in UploadPersistence implementations and
+ * from there passes through UploadManager implementations and back to the
+ * caller. It is also thrown directly by some UploadManager implementations.
  * </p>
- *
- * @author aubergineanode, singlewood
+ * @author aubergineanode, TCSDEVELOPER
  * @version 1.0
  */
-public class UploadPersistenceException extends BaseException {
+public class UploadPersistenceException extends PersistenceException {
 
     /**
      * Creates a new UploadPersistenceException.
-     *
-     * @param message Explanation of error, can be null
+     * @param message
+     *            Explanation of error, can be null
      */
     public UploadPersistenceException(String message) {
         super(message);
@@ -34,9 +31,10 @@ public class UploadPersistenceException extends BaseException {
 
     /**
      * Creates a new UploadPersistenceException.
-     *
-     * @param message Explanation of error, can be null
-     * @param cause Underlying cause of error, can be null
+     * @param message
+     *            Explanation of error, can be null
+     * @param cause
+     *            Underlying cause of error, can be null
      */
     public UploadPersistenceException(String message, Exception cause) {
         super(message, cause);
