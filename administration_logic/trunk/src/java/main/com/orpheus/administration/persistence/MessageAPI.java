f8130b68f9aac5e34c24c18f0da51deb57abe949
jbollinger <jbollinger@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-12-05 20:39:31 +0000

    Removed stubs for the Administration Persistence classes
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@44733 fb370eea-3af6-4597-97f7-f7400a59c12a

--- administration_logic/trunk/src/java/main/com/orpheus/administration/persistence/MessageAPI.java
+++
@@ -1,5 +0,0 @@
-package com.orpheus.administration.persistence;
-
-public class MessageAPI {
-
-}
--- administration_logic/trunk/src/java/main/com/orpheus/administration/persistence/SearchCriteria.java
+++
@@ -1,9 +0,0 @@
-
-package com.orpheus.administration.persistence;
-
-/**
- * 
- * 
- */
-public class SearchCriteria {
- }
