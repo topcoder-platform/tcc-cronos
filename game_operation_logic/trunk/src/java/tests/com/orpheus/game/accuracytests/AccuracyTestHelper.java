769674a160ce209d439d6bdcd4ab928fecf562ad
jbollinger <jbollinger@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-12-15 16:09:11 +0000

    Fixed accuracy tests to get an DOM parser via JAXP for compatibility.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@45762 fb370eea-3af6-4597-97f7-f7400a59c12a

--- game_operation_logic/trunk/src/java/tests/com/orpheus/game/accuracytests/AccuracyTestHelper.java
+++ game_operation_logic/trunk/src/java/tests/com/orpheus/game/accuracytests/AccuracyTestHelper.java
@@ -3,13 +3,16 @@
  */
 package com.orpheus.game.accuracytests;
 
-import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 import com.topcoder.util.config.ConfigManager;
 
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Iterator;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -21,6 +24,16 @@ import org.xml.sax.SAXException;
  * @version 1.0
  */
 final class AccuracyTestHelper {
+
+    /**
+     * A DocumentBuilderFactory used to generate XML parsers in XML2Element
+     */
+    private final static DocumentBuilderFactory dbFactory;
+
+    static {
+        dbFactory = DocumentBuilderFactory.newInstance();
+        dbFactory.setValidating(false);
+    }
 
 	/**
 	 * Emtpy private constructor to make this class can not be instanced.
@@ -35,12 +48,12 @@ final class AccuracyTestHelper {
 	 */
 	static Element XML2Element(String content) throws Exception {
 		// Create a local parser for thread-safety
-		DOMParser parser = new DOMParser();
+		DocumentBuilder parser = dbFactory.newDocumentBuilder();
 		StringReader reader = new StringReader(content);
 		InputSource inputSource = new InputSource(reader);
+		Document doc = parser.parse(inputSource);
 
-		parser.parse(inputSource);
-		return parser.getDocument().getDocumentElement();
+		return doc.getDocumentElement();
 	}
 
 	/**
