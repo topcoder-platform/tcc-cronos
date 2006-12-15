e7badba969bf54a7b6037c5d04ad6d14b12a988f
jbollinger <jbollinger@fb370eea-3af6-4597-97f7-f7400a59c12a>
2006-12-15 16:02:59 +0000

    Restored the correct version of AllTests.java -- all but the accuracy tests had been removed from the most recent version.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@45760 fb370eea-3af6-4597-97f7-f7400a59c12a

--- game_operation_logic/trunk/src/java/tests/com/orpheus/game/AllTests.java
+++ game_operation_logic/trunk/src/java/tests/com/orpheus/game/AllTests.java
@@ -9,6 +9,8 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import com.orpheus.game.accuracytests.AccuracyTests;
+import com.orpheus.game.failuretests.FailureTests;
+import com.orpheus.game.stresstests.StressTests;
 
 /**
  * <p>This test case aggregates all Unit test cases.</p>
@@ -20,7 +22,19 @@ public class AllTests extends TestCase {
 
     public static Test suite() {
         final TestSuite suite = new TestSuite();
+        
+        //unit tests
+        suite.addTest(UnitTests.suite());
+        
+        //accuracy tests
         suite.addTest(AccuracyTests.suite());
+        
+        //failure tests
+        suite.addTest(FailureTests.suite());
+        
+        //stress tests
+        suite.addTest(StressTests.suite());
+        
         return suite;
     }
 
