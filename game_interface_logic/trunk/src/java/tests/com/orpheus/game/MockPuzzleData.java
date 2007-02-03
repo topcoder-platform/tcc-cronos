d2f84d6ccfac8d2c09ba05decbd9bd4eb09b2707
waits <waits@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-02-03 08:48:51 +0000

    for ball 5242
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@50678 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ game_interface_logic/trunk/src/java/tests/com/orpheus/game/MockPuzzleData.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2006 TopCoder Inc., All Rights Reserved.
+ */
+package com.orpheus.game;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.topcoder.util.puzzle.PuzzleData;
+import com.topcoder.util.puzzle.PuzzleResource;
+
+/**
+ * <p>
+ * Mock implementation of <code>{@link com.topcoder.util.puzzle.PuzzleData}</code> method.
+ * </p>
+ * @author FireIce
+ * @version 1.0
+ */
+public class MockPuzzleData implements PuzzleData {
+
+    /**
+     * <p>
+     * Mock method.
+     * </p>
+     */
+    public String getName() {
+        return "test";
+    }
+
+    /**
+     * <p>
+     * Mock method.
+     * </p>
+     */
+    public String getAttribute(String name) {
+        return "test";
+    }
+
+    /**
+     * <p>
+     * Mock method.
+     * </p>
+     */
+    public Map getAllAttributes() {
+        return new HashMap();
+    }
+
+    /**
+     * <p>
+     * Mock method.
+     * </p>
+     */
+    public PuzzleResource getResource(String name) {
+        return null;
+    }
+
+    /**
+     * <p>
+     * Mock method.
+     * </p>
+     */
+    public Map getAllResources() {
+        return null;
+    }
+
+}
