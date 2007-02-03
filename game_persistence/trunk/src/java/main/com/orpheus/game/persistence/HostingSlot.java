4137def4ac4ad84fb28382ef0df8057ea5b41938
waits <waits@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-02-03 08:56:53 +0000

    update
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@50681 fb370eea-3af6-4597-97f7-f7400a59c12a

--- game_persistence/trunk/src/java/main/com/orpheus/game/persistence/HostingSlot.java
+++ game_persistence/trunk/src/java/main/com/orpheus/game/persistence/HostingSlot.java
@@ -93,4 +93,17 @@ public interface HostingSlot extends Serializable {
      * @return the end date of the hosting
      */
     Date getHostingEnd();
+    
+    /**
+     * Returns the corresponding bid id of the slot.
+     * 
+     * @return the corresponding bid id
+     */
+    long getBidId();
+    
+    /**
+     * Set bid id.
+     * 
+     */
+    void setBidId(long bidId);
 }
--- game_persistence/trunk/src/java/main/com/orpheus/game/persistence/entities/HostingSlotImpl.java
+++ game_persistence/trunk/src/java/main/com/orpheus/game/persistence/entities/HostingSlotImpl.java
@@ -102,6 +102,13 @@ public class HostingSlotImpl implements HostingSlot {
      * </p>
      */
     private final Domain domain;
+    
+    /**
+     * <p>
+     * The bid id.//FIXME for fix ball 5242, add the bid id.
+     * </p>
+     */
+    private long bidId = 0;
 
     /**
      * <p>
@@ -281,5 +288,20 @@ public class HostingSlotImpl implements HostingSlot {
      */
     public Date getHostingEnd() {
         return this.hostingEnd;
+    }
+
+    /**
+     * set the bid id.
+     * @param bidId the bid id
+     */
+    public void setBidId(long bidId){
+        this.bidId = bidId;
+    }
+    /**
+     * get bid id.
+     * @see com.orpheus.game.persistence.HostingSlot#getBidId()
+     */
+    public long getBidId() {
+        return this.bidId;
     }
 }
