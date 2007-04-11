63eb99d9e1fe55c21b1b0f2956f4464ab3314d9c
enefem21 <enefem21@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-04-11 16:17:57 +0000

    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@57874 fb370eea-3af6-4597-97f7-f7400a59c12a

--- time_tracker_report/trunk/test_files/demoWebapp/web/js/gui.js
+++
@@ -1,5 +0,0 @@
-/**
-* @package mh.gui
-* Provides the classes necessary to create gui components.
-*/
-js.gui = {};
--- time_tracker_report/trunk/test_files/demoWebapp/web/js/parseEmail.js
+++
@@ -1,4 +0,0 @@
-function isEmail(s)
-{
-	return !(null == s.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi));
-}
