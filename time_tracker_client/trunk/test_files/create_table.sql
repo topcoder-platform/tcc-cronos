7a7d4b2ad9779be6229198af084d8da3f89a9a7d
qiucx0161 <qiucx0161@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-04-19 14:10:29 +0000

    add script
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@58922 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ time_tracker_client/trunk/test_files/create_table.sql
@@ -0,0 +1,52 @@
+create table client  (
+  client_id            INTEGER                         not null,
+  name                 VARCHAR(64)                     not null,
+  company_id           INTEGER                         not null,
+  creation_date        DATETIME YEAR TO SECOND         not null,
+  creation_user        VARCHAR(64)                     not null,
+  modification_date    DATETIME YEAR TO SECOND         not null,
+  modification_user    VARCHAR(64)                     not null,
+  payment_term_id      INTEGER                         not null,
+  status               smallint                        not null,
+  salesTax             decimal(7, 3)                   not null,
+  start_date           DATETIME YEAR TO SECOND         not null,
+  end_date             DATETIME YEAR TO SECOND         not null,
+primary key (client_id)
+      constraint pk_client
+);
+
+create table project  (
+  project_id           INTEGER                         not null,
+  name                 VARCHAR(64)                     not null,
+  company_id           INTEGER                         not null,
+  description          VARCHAR(255),
+  start_date           DATETIME YEAR TO SECOND         not null,
+  end_date             DATETIME YEAR TO SECOND         not null,
+  creation_date        DATETIME YEAR TO SECOND         not null,
+  creation_user        VARCHAR(64)                     not null,
+  modification_date    DATETIME YEAR TO SECOND         not null,
+  modification_user    VARCHAR(64)                     not null,
+primary key (project_id)
+      constraint pk_project
+);
+
+
+create table client_project  (
+  client_id            INTEGER                         not null,
+  project_id           INTEGER                         not null,
+  creation_date        DATETIME YEAR TO SECOND         not null,
+  creation_user        VARCHAR(64)                     not null,
+  modification_date    DATETIME YEAR TO SECOND         not null,
+  modification_user    VARCHAR(64)                     not null,
+primary key (client_id, project_id)
+      constraint pk_client_project
+);
+
+create table id_sequences  (
+  name               VARCHAR(255)                      not null,
+  next_block_start     INTEGER                         not null,
+  block_size           INTEGER                         not null,
+  exhausted            INTEGER                         not null,
+primary key (name)
+      constraint pk_id_sequences
+);
