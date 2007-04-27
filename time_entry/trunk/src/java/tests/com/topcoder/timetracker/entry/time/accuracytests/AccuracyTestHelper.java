4f3cf7a65bba89664a94cf1b8207059880e3c58b
enefem21 <enefem21@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-04-27 14:02:08 +0000

    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@59911 fb370eea-3af6-4597-97f7-f7400a59c12a

--- time_entry/trunk/src/java/tests/com/topcoder/timetracker/entry/time/accuracytests/AccuracyTestHelper.java
+++ time_entry/trunk/src/java/tests/com/topcoder/timetracker/entry/time/accuracytests/AccuracyTestHelper.java
@@ -313,6 +313,8 @@ public class AccuracyTestHelper {
 
         timeEntry.setInvoiceId(1);
         timeEntry.setCompanyId(1);
+        timeEntry.setClientId(1);
+        timeEntry.setProjectId(1);
         timeEntry.setHours(13.2);
         timeEntry.setBillable(true);
 
