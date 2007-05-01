43968c275aa2955761544f2104bb862d0702b8ee
kshatriyan <kshatriyan@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-05-01 18:32:41 +0000

    Removed in version 1.1
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@60441 fb370eea-3af6-4597-97f7-f7400a59c12a

--- project_management_persistence/trunk/test_files/stresstests/delete.sql
+++
@@ -1,7 +0,0 @@
-delete from project_audit;
-delete from project_info;
-delete from project;
-delete from project_category_lu;
-delete from project_info_type_lu;
-delete from project_status_lu;
-delete from project_type_lu;
