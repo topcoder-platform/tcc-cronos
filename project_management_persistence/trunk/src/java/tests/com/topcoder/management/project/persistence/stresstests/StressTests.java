64594ddfe4cff8b20e643abd4ecd01c5771a57a2
kshatriyan <kshatriyan@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-05-01 18:22:48 +0000

    From version 1.1
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@60434 fb370eea-3af6-4597-97f7-f7400a59c12a

--- project_management_persistence/trunk/src/java/tests/com/topcoder/management/project/persistence/stresstests/StressTests.java
+++ project_management_persistence/trunk/src/java/tests/com/topcoder/management/project/persistence/stresstests/StressTests.java
@@ -1,5 +1,5 @@
-/**
- * Copyright (c) 2006, TopCoder, Inc. All rights reserved
+/*
+ * Copyright (C) 2007 TopCoder Inc., All Rights Reserved.
  */
 package com.topcoder.management.project.persistence.stresstests;
 
@@ -7,27 +7,27 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-
-
 /**
  * <p>
- * This test case aggregates all Stress test cases.
+ * This test case aggregates all stress test cases for Project Management Persistence 1.1.
  * </p>
  *
- * @author mgmg
- * @version 1.0
+ * @author Hacker_QC
+ * @version 1.1
  */
 public class StressTests extends TestCase {
+
     /**
      * <p>
-     * Return all tests.
+     * Creates a test suite of the tests contained in this class.
      * </p>
      *
-     * @return all tests
+     * @return a test suite of the tests contained in this class.
      */
     public static Test suite() {
         final TestSuite suite = new TestSuite();
-        suite.addTestSuite(InformixProjectPersistenceStressTest.class);
+
+        suite.addTestSuite(PersistenceStressTest.class);
 
         return suite;
     }
