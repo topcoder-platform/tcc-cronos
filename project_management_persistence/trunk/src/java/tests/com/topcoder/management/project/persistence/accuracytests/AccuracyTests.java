9166fe11db495a5fdde61e081f9e4370487df9fc
kshatriyan <kshatriyan@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-05-01 18:19:11 +0000

    From version 1.1
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@60432 fb370eea-3af6-4597-97f7-f7400a59c12a

--- project_management_persistence/trunk/src/java/tests/com/topcoder/management/project/persistence/accuracytests/AccuracyTests.java
+++ project_management_persistence/trunk/src/java/tests/com/topcoder/management/project/persistence/accuracytests/AccuracyTests.java
@@ -1,25 +1,33 @@
-/**
- *
- * Copyright (c) 2006, TopCoder, Inc. All rights reserved
+/*
+ * Copyright (C) 2007 TopCoder Inc., All Rights Reserved.
  */
 package com.topcoder.management.project.persistence.accuracytests;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import junit.framework.TestResult;
 
 /**
  * <p>This test case aggregates all Accuracy test cases.</p>
  *
- * @author TopCoder
+ * @author TCSDEVELOPER
  * @version 1.0
  */
 public class AccuracyTests extends TestCase {
-
+    /**
+     * <p>
+     * This test case aggregates all Accuracy test cases.
+     * </p>
+     *
+     * @return all Accuracy test cases.
+     */
     public static Test suite() {
         final TestSuite suite = new TestSuite();
-        suite.addTestSuite(InformixProjectPersistenceAccuracyTest.class);;
+
+        suite.addTest(AbstractInformixProjectPersistenceAccuracyTests.suite());
+        suite.addTest(InformixProjectPersistenceAccuracyTests.suite());
+        suite.addTest(UnmanagedTransactionInformixProjectPersistenceAccuracyTests.suite());
+
         return suite;
     }
 
