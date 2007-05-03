a4c5934cc97946124200555d1a85bfbfa0bac06b
mittu <mittu@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-05-03 22:31:06 +0000

    Missed files added.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@60828 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ im_login/trunk/src/java/tests/com/cronos/im/login/AllTests.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * Copyright (c) 2007, TopCoder, Inc. All rights reserved
+ */
+package com.cronos.im.login;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import com.cronos.im.login.accuracytests.AccuracyTests;
+import com.cronos.im.login.failuretests.FailureTests;
+import com.cronos.im.login.stresstests.StressTests;
+
+/**
+ * <p>
+ * This test case aggregates all test cases.
+ * </p>
+ *
+ * @author TopCoder
+ * @version 1.0
+ */
+public class AllTests extends TestCase {
+
+    public static Test suite() {
+        final TestSuite suite = new TestSuite();
+
+        // unit tests
+        suite.addTest(UnitTests.suite());
+
+        // accuracy tests
+         suite.addTest(AccuracyTests.suite());
+
+        // failure tests
+        suite.addTest(FailureTests.suite());
+
+        // stress tests
+         suite.addTest(StressTests.suite());
+
+        return suite;
+    }
+
+}
