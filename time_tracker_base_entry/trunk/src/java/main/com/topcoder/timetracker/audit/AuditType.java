2f88f34a3c7782cf080cf3579f238c5a8d0a7a7b
George1 <George1@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-05-10 22:13:26 +0000

    Removed unneeded classes.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@61629 fb370eea-3af6-4597-97f7-f7400a59c12a

--- time_tracker_base_entry/trunk/src/java/main/com/topcoder/timetracker/audit/AuditType.java
+++
@@ -1,8 +0,0 @@
-package com.topcoder.timetracker.audit;
-
-public interface AuditType {
-
-    public final static int INSERT = 1;
-    public final static int UPDATE = 2;
-    public final static int DELETE = 3;
-}
