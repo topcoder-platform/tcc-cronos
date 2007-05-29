78671015f9e70d3e306e14f93abab5a0d60574b5
pnussembaum <pnussembaum@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-05-29 16:57:03 +0000

    modfication to retrieve from the session the URL to redirect users after a successful login
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@63739 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ online_review_login/tags/version-1.0.1.0/src/java/tests/com/cronos/onlinereview/login/accuracytests/AccuracyTests.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2006 TopCoder Inc., All Rights Reserved.
+ */
+package com.cronos.onlinereview.login.accuracytests;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+/**
+ * <p>
+ * This test case aggregates all Accuracy test cases.
+ * </p>
+ *
+ * @author PE
+ * @version 1.0
+ */
+public class AccuracyTests extends TestCase {
+    /**
+     * <p>
+     * Creates a test suite of the tests contained in this class.
+     * </p>
+     *
+     * @return a test suite of the tests contained in this class.
+     */
+    public static Test suite() {
+        final TestSuite suite = new TestSuite();
+
+        suite.addTestSuite(SecurityManagerAuthResponseParserAccuracyTest.class);
+        suite.addTestSuite(SecurityManagerAuthenticatorAccuracyTest.class);
+        suite.addTestSuite(LoginActionsAccuracyTest.class);
+
+        return suite;
+    }
+}
