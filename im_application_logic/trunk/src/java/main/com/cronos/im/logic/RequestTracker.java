93ed80261bc660c88389c1402627be881c666159
j3_guile <j3_guile@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-07-19 15:14:23 +0000

    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@70621 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ im_application_logic/trunk/src/java/main/com/cronos/im/logic/RequestTracker.java
@@ -0,0 +1,40 @@
+package com.cronos.im.logic;
+
+import java.util.HashSet;
+
+public class RequestTracker {
+    
+    private class UserKey {
+        private final long userId;
+        
+        private final long categoryId;
+        
+        public UserKey(long userId, long categoryId) {
+            super();
+            this.userId = userId;
+            this.categoryId = categoryId;
+        }
+    }
+    
+    private static RequestTracker instance = new RequestTracker();
+    
+    private static HashSet requesters = new HashSet();
+
+    private RequestTracker() {}
+
+    public static RequestTracker getInstance() {
+        return instance;
+    }
+    
+    public void addRequester(long userId, long categoryId) {
+        requesters.add(new UserKey(userId, categoryId));
+    }
+    
+    public boolean isRequesterPresent(long userId, long categoryId) {
+        return requesters.contains(new UserKey(userId, categoryId));
+    }
+    
+    public boolean removeRequester(long userId, long categoryId) {
+        return requesters.remove(new UserKey(userId, categoryId));
+    }
+}
