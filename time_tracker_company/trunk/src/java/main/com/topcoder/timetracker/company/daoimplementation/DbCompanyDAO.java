a5b86a19c2138cacac442b895d9b6bee4d58fcf3
George1 <George1@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-07-24 14:41:20 +0000

    Transaction isolation level is now set on every connection.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@71160 fb370eea-3af6-4597-97f7-f7400a59c12a

--- time_tracker_company/trunk/src/java/main/com/topcoder/timetracker/company/daoimplementation/DbCompanyDAO.java
+++ time_tracker_company/trunk/src/java/main/com/topcoder/timetracker/company/daoimplementation/DbCompanyDAO.java
@@ -1534,11 +1534,17 @@ public class DbCompanyDAO implements CompanyDAO {
      * @throws CompanyDAOException If can't get connection.
      */
     private Connection createConnection() throws CompanyDAOException {
+        Connection con = null;
         try {
             // create a DB connection
-            return connectionFactory.createConnection(connectionName);
+            con = connectionFactory.createConnection(connectionName);
+            con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+            return con;
         } catch (DBConnectionException e) {
             throw new CompanyDAOException("Can't get the connection from database.", e, null);
+        } catch (SQLException e) {
+            releaseConnection(con);
+            throw new CompanyDAOException("Can't set transaction isolation for connection.", e, null);
         }
     }
 
