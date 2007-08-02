901be645afdbef7042a00a7169ea71e358fbcf8f
mtong <mtong@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-08-02 20:54:55 +0000

    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@72374 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ ball/Forums_0.1/src/java/main/com/topcoder/web/ejb/forums/ForumsBean.java
@@ -0,0 +1,171 @@
+package com.topcoder.web.ejb.forums;
+
+import com.jivesoftware.base.*;
+import com.jivesoftware.forum.*;
+import com.topcoder.shared.util.DBMS;
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.web.ejb.BaseEJB;
+import com.topcoder.web.forums.ForumConstants;
+import com.topcoder.web.forums.model.TCAuthToken;
+
+import javax.ejb.EJBException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.*;
+
+/**
+ * This class handles interaction with the Jive database.
+ * Please update the code if you know of a way to use Jive objects remotely, instead of using primitives.
+ * Keep in mind that most Jive proxy objects are not serializable.
+ *
+ * @author mtong
+ */
+
+public class ForumsBean extends BaseEJB {
+
+    private static Logger log = Logger.getLogger(ForumsBean.class);
+    private static ForumFactory forumFactory = ForumFactory.getInstance(new TCAuthToken(1));
+
+    public String[] getCategoryNames() {
+        ForumFactory forumFactory = ForumFactory.getInstance(AuthFactory.getAnonymousAuthToken());
+        Iterator it = forumFactory.getRootForumCategory().getCategories();
+        ArrayList listNames = new ArrayList();
+        while (it.hasNext()) {
+            listNames.add(((ForumCategory) it.next()).getName());
+        }
+        return (String[]) listNames.toArray(new String[listNames.size()]);
+    }
+
+    public int getThreadMessageCount(int threadID) {
+        return this.selectInt("jivemessage",
+                "count(*)",
+                new String[]{"threadid"},
+                new String[]{String.valueOf(threadID)},
+                DBMS.FORUMS_DATASOURCE_NAME).intValue();
+    }
+
+    public void closeCategory(long categoryID) throws ForumCategoryNotFoundException, UnauthorizedException {
+        ForumCategory category = forumFactory.getForumCategory(categoryID);
+        category.setProperty(ForumConstants.PROPERTY_ARCHIVAL_STATUS, String.valueOf(ForumConstants.PROPERTY_ARCHIVAL_STATUS_CLOSED));
+    }
+
+    public boolean canReadCategory(long userID, long categoryID) throws ForumCategoryNotFoundException {
+        ForumFactory userForumFactory = ForumFactory.getInstance(new TCAuthToken(userID));
+        ForumCategory category = userForumFactory.getForumCategory(categoryID);
+        return category.isAuthorized(ForumPermissions.READ_FORUM);
+    }
+
+    public void createCategoryWatch(long userID, long categoryID) throws ForumCategoryNotFoundException, UnauthorizedException, UserNotFoundException {
+        WatchManager watchManager = forumFactory.getWatchManager();
+        User user = forumFactory.getUserManager().getUser(userID);
+        ForumCategory category = forumFactory.getForumCategory(categoryID);
+        if (!watchManager.isWatched(user, category)) {
+            watchManager.createWatch(user, category);
+        }
+    }
+
+    public void createCategoryWatches(long userID, long[] categoryIDs) throws ForumCategoryNotFoundException, UnauthorizedException, UserNotFoundException {
+        WatchManager watchManager = forumFactory.getWatchManager();
+        User user = forumFactory.getUserManager().getUser(userID);
+        for (int i = 0; i < categoryIDs.length; i++) {
+            ForumCategory category = forumFactory.getForumCategory(categoryIDs[i]);
+            if (!watchManager.isWatched(user, category)) {
+                watchManager.createWatch(user, category);
+            }
+        }
+    }
+
+    public void deleteCategoryWatch(long userID, long categoryID) throws ForumCategoryNotFoundException, UnauthorizedException, UserNotFoundException {
+        WatchManager watchManager = forumFactory.getWatchManager();
+        User user = forumFactory.getUserManager().getUser(userID);
+        ForumCategory category = forumFactory.getForumCategory(categoryID);
+        Watch watch = watchManager.getWatch(user, category);
+        if (watch != null) {
+            watchManager.deleteWatch(watch);
+        }
+    }
+
+    public void deleteOrphanedAttachments() {
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = DBMS.getConnection(DBMS.FORUMS_DATASOURCE_NAME);
+            ps = conn.prepareStatement(
+                    "delete from jiveattachment where objectid is null");
+            ps.executeUpdate();
+            log.info("Successfully deleted orphaned attachments.");
+        } catch (SQLException e) {
+            DBMS.printSqlException(true, e);
+            throw new EJBException(e.getMessage());
+        } catch (Exception e) {
+            throw new EJBException(e.getMessage());
+        } finally {
+            close(ps);
+            close(conn);
+        }
+    }
+
+    private void logException(Exception e, String msg) {
+        log.info("*** " + msg + ": " + e);
+        StackTraceElement[] ste = e.getStackTrace();
+        for (int i = 0; i < ste.length; i++) {
+            log.info(ste[i]);
+        }
+    }
+}
+
+class JiveForumCategoryComparator implements Comparator {
+    private String sortField;
+    private int sortOrder;
+
+    public JiveForumCategoryComparator(String sortField, int sortOrder) {
+        this.sortField = sortField;
+        this.sortOrder = sortOrder;
+    }
+
+    public final int compare(Object o1, Object o2) {
+        ForumCategory c1 = (ForumCategory) o1;
+        ForumCategory c2 = (ForumCategory) o2;
+
+        int retVal = 0;
+        if (sortField.equals("id")) {
+            retVal = String.valueOf(c1.getID()).compareTo(String.valueOf(c2.getID()));
+        } else if (sortField.equals("name")) {
+            retVal = String.valueOf(c1.getName()).compareTo(String.valueOf(c2.getName()));
+        }
+        if (sortOrder == ResultFilter.DESCENDING) {
+            retVal = -retVal;
+        }
+        return retVal;
+    }
+}
+
+class JiveGroupComparator implements Comparator {
+    private String sortField;
+    private int sortOrder;
+
+    public JiveGroupComparator(String sortField, int sortOrder) {
+        this.sortField = sortField;
+        this.sortOrder = sortOrder;
+    }
+
+    public final int compare(Object o1, Object o2) {
+        Group g1 = (Group) o1;
+        Group g2 = (Group) o2;
+
+        int retVal = 0;
+        if (sortField.equals("id")) {
+            retVal = String.valueOf(g1.getID()).compareTo(String.valueOf(g2.getID()));
+        } else if (sortField.equals("name")) {
+            retVal = g1.getName().compareTo(g2.getName());
+        } else if (sortField.equals("description")) {
+            retVal = g1.getDescription().compareTo(g2.getDescription());
+        }
+        if (sortOrder == ResultFilter.DESCENDING) {
+            retVal = -retVal;
+        }
+        return retVal;
+    }
+}
---
+++ ball/Forums_0.1/src/java/main/com/topcoder/web/forums/controller/ForumsServlet.java
@@ -0,0 +1,219 @@
+/*
+ * Created on Apr 27, 2005
+ */
+package com.topcoder.web.forums.controller;
+
+import com.jivesoftware.base.AuthFactory;
+import com.jivesoftware.base.AuthToken;
+import com.jivesoftware.base.UnauthorizedException;
+import com.jivesoftware.forum.ForumFactory;
+import com.orpheus.game.server.OrpheusFunctions;
+import com.topcoder.shared.util.TCContext;
+import com.topcoder.shared.util.logging.Logger;
+import com.topcoder.user.profile.UserProfile;
+import com.topcoder.web.common.*;
+import com.topcoder.web.common.error.RequestRateExceededException;
+import com.topcoder.web.ejb.forums.ForumsLocal;
+import com.topcoder.web.ejb.forums.ForumsLocalHome;
+import com.topcoder.web.forums.controller.request.ForumsProcessor;
+
+import javax.naming.Context;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author mtong
+ *         <p/>
+ *         Provides session and authentication information to the forum's processors.
+ */
+public class ForumsServlet extends BaseServlet {
+    private final static Logger log = Logger.getLogger(ForumsServlet.class);
+
+    public synchronized void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        // Delete orphaned attachments daily
+        Thread tOrphaned = new Thread() {
+            public void run() {
+                while (true) {
+                    try {
+                        log.info("Deleting orphaned attachments...");
+                        ForumsLocal forumsBean = getForumsBean();
+                        if (forumsBean != null) {
+                            forumsBean.deleteOrphanedAttachments();
+                        } else {
+                            log.error("Could not delete orphaned attachments: forumsBean is null");
+                        }
+                        sleep(86400000);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        tOrphaned.start();
+    }
+
+    protected void process(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+
+        RequestProcessor rp = null;
+
+        try {
+            try {
+                request.setCharacterEncoding("utf-8");
+                if (log.isDebugEnabled()) {
+                    log.debug("content type: " + request.getContentType());
+                }
+
+                AuthToken authToken = AuthFactory.getAnonymousAuthToken();
+                try {
+                    authToken = AuthFactory.getAuthToken(request, response);
+                } catch (UnauthorizedException uae) {
+                }
+
+                TCRequest tcRequest = HttpObjectFactory.createRequest(request);
+                TCResponse tcResponse = HttpObjectFactory.createResponse(response);
+
+                UserProfile userProfile = (UserProfile)request.getSession(true).getAttribute("user_profile");
+                String username = OrpheusFunctions.getHandle(userProfile);
+                
+                String servletPath = request.getContextPath() + request.getServletPath();
+                String query = request.getQueryString();
+                String queryString = (query == null) ? ("") : ("?" + query);
+                StringBuffer buf = new StringBuffer(200);
+                buf.append("http://");
+                buf.append(request.getServerName());
+                buf.append(servletPath);
+                buf.append(queryString);
+                String requestString = buf.toString();
+                
+                if (throttleEnabled) {
+                    if (throttle.throttle(request.getSession().getId())) {
+                        throw new RequestRateExceededException(request.getSession().getId(), username);
+                    }
+                }
+
+                //we can let browsers/proxies cache pages if the user is anonymous or it's https (they don't really cache https setuff)
+                if (!authToken.isAnonymous() &&
+                        !request.getRequestURL().toString().toLowerCase().startsWith("https")) {
+                    log.debug("using an uncached response");
+                    tcResponse = HttpObjectFactory.createUnCachedResponse(response);
+                }
+
+                com.jivesoftware.base.User forumUser = null;
+                ForumFactory forumFactory = ForumFactory.getInstance(authToken);
+                if (!authToken.isAnonymous()) {
+                    forumUser = forumFactory.getUserManager().getUser(authToken.getUserID());
+                }
+
+                StringBuffer loginfo = new StringBuffer(100);
+                loginfo.append("[* ");
+                loginfo.append(username);
+                loginfo.append(" * ");
+                loginfo.append(request.getRemoteAddr());
+                loginfo.append(" * ");
+                loginfo.append(request.getMethod());
+                loginfo.append(" ");
+                loginfo.append(requestString);
+                loginfo.append(" *]");
+                log.info(loginfo);
+
+                try {
+                    String cmd = StringUtils.checkNull((String) tcRequest.getAttribute(MODULE));
+                    if (cmd.equals(""))
+                        cmd = StringUtils.checkNull(tcRequest.getParameter(MODULE));
+
+                    if (cmd.equals(""))
+                        cmd = DEFAULT_PROCESSOR;
+                    if (!isLegalCommand(cmd))
+                        throw new NavigationException();
+
+                    String processorName = getProcessor(cmd);
+                    if (processorName.indexOf(".") == -1) {
+                        processorName = PATH + (PATH.endsWith(".") ? "" : ".") + processorName;
+                    }
+
+                    if (log.isDebugEnabled()) {
+                        log.debug("creating request processor for " + processorName);
+                    }
+                    try {
+                        rp = callProcess(processorName, request, response, tcRequest, tcResponse, authToken, forumUser, forumFactory);
+                    } catch (ClassNotFoundException e) {
+                        throw new NavigationException("Invalid request", e);
+                    }
+                } catch (PermissionException pe) {
+                    if (authToken.isAnonymous()) {
+                        log.info(username + " does not have access to " + pe.getResource().getName() + " sending to login");
+                        handleLogin(request, response, request.getServerName());
+                        return;
+                    } else {
+                        log.info(username + " does not have access to " + pe.getResource().getName() + " sending to error");
+                        throw pe;
+                    }
+                }
+                if (!response.isCommitted()) {
+                    fetchRegularPage(request, response, rp.getNextPage(), rp.isNextPageInContext());
+                    return;
+                }
+            } catch (Throwable e) {
+                handleException(request, response, e);
+            }
+
+            /* things are extremely broken, or perhaps some of the response
+             * buffer had already been flushed when an error was thrown,
+             * and the forward to error page failed.  in any event, make
+             * one last attempt to get an error message to the browser
+             */
+        } catch (Throwable e) {
+            log.fatal("forwarding to error page failed", e);
+            e.printStackTrace();
+            response.setContentType("text/html");
+            response.setStatus(500);
+            PrintWriter out = response.getWriter();
+            out.println("<html><head><title>Internal Error</title></head>");
+            out.println("<body><h4>Your request could not be processed.  Please inform The Ball.</h4>");
+            out.println("</body></html>");
+            out.flush();
+        }
+    }
+
+    protected void handleLogin(HttpServletRequest request, HttpServletResponse response, String serverName) throws Exception {
+        /* forward to the login page 
+         * TODO: redirect back to the original page */
+        StringBuffer nextPage = new StringBuffer("http://").append(serverName).append(LOGIN_SERVLET).append(LOGIN_PROCESSOR);
+        fetchRegularPage(request, response, nextPage.toString(), false);
+    }
+
+    protected RequestProcessor callProcess(String processorName, HttpServletRequest httpRequest,
+                                           HttpServletResponse httpResponse, TCRequest request, TCResponse response,
+                                           AuthToken authToken, com.jivesoftware.base.User user,
+                                           ForumFactory factory) throws Exception {
+        ForumsProcessor rp = null;
+
+        rp = (ForumsProcessor) Class.forName(processorName).newInstance();
+        rp.setRequest(request);
+        rp.setResponse(response);
+        rp.setAuthToken(authToken);
+        rp.setUser(user);
+        rp.setForumFactory(factory);
+        rp.process();
+        return rp;
+    }
+
+    private ForumsLocal getForumsBean() {
+        ForumsLocal forumsBean = null;
+        try {
+            Context context = TCContext.getInitial();
+            ForumsLocalHome forumsLocalHome = (ForumsLocalHome) context.lookup(ForumsLocalHome.EJB_REF_NAME);
+            forumsBean = forumsLocalHome.create();
+        } catch (Exception e) {
+            log.error(e);
+        }
+        return forumsBean;
+    }
+}
