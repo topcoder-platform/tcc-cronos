76381be95f79fa596204d840ae76e70bcd7b8ba3
j3_guile <j3_guile@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-08-03 16:11:46 +0000

    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@72468 fb370eea-3af6-4597-97f7-f7400a59c12a

--- im_persistence/trunk/src/java/main/com/cronos/im/persistence/InformixEntityStatusTracker.java
+++ im_persistence/trunk/src/java/main/com/cronos/im/persistence/InformixEntityStatusTracker.java
@@ -249,10 +249,10 @@ public class InformixEntityStatusTracker extends AbstractPersistenceWithValidato
                 try {
                     List statuses = new ArrayList();
                     while (results.next()) {
-                        Long resultId = results.getLong(type + "_id");
+                        long resultId = results.getLong(type + "_id");
                         EntityKey locKey = null;
                         for (int i = 0; i < keys.length; i++) {
-                            if(resultId != null && resultId.longValue() ==  getPrimaryId(keys[i])) {
+                            if(resultId ==  getPrimaryId(keys[i])) {
                                 locKey = keys[i];
                                 break;
                             }
