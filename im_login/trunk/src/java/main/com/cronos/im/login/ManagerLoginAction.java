69984a56ba8acecce4916814de0bbb4f1312a0da
colau <colau@fb370eea-3af6-4597-97f7-f7400a59c12a>
2007-09-14 14:38:29 +0000

    updated
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@77197 fb370eea-3af6-4597-97f7-f7400a59c12a

--- im_login/trunk/src/java/main/com/cronos/im/login/ManagerLoginAction.java
+++ im_login/trunk/src/java/main/com/cronos/im/login/ManagerLoginAction.java
@@ -154,7 +154,14 @@ public class ManagerLoginAction extends LoginAction {
         // Get the principal from persistence.
         AuthorizationPersistence persistence = getAuthorizationManager().getPersistence();
         Collection temp = persistence.searchPrincipals(user);
-        Principal principal = (Principal) temp.iterator().next();
+        Principal principal = null;
+        
+        if(temp != null && temp.iterator().hasNext()) {
+            principal = (Principal) temp.iterator().next();
+        } else {
+            doLog(user, ACTION_NAME, "" + subject.getUserId(), "Failed to authorize the manager user, no Principal found!", Level.WARN);
+            return mapping.findForward(getLoginAuthFailForwardName());
+        }
 
         // authorize the user
         if (getAuthorizationManager().authorize(principal, getAction(), getActionContext())) {
