650cefc8e260f16cda94a41091644949d7f07df1
King_Bette <King_Bette@fb370eea-3af6-4597-97f7-f7400a59c12a>
2008-05-25 12:35:07 +0000

    QC done, and fix build problem.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@116590 fb370eea-3af6-4597-97f7-f7400a59c12a

--- dotnet_distance_generator/trunk/src/csharp/main/TopCoder/Web/Distance/AssemblyInfo.cs
+++ dotnet_distance_generator/trunk/src/csharp/main/TopCoder/Web/Distance/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("http://www.topcoder.com")]
 [assembly: AssemblyProduct("TopCoder .NET Component Catalog")]
-[assembly: AssemblyCopyright("Copyright © 2008, TopCoder, Inc. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright ?2008, TopCoder, Inc. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0")]
@@ -15,5 +15,5 @@ using System.Runtime.CompilerServices;
 // executable use the NAnt build file. To build under Visual Studio just
 // exclude this file from the build.
 [assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile(@"..\tcs\bin\TopCoder.snk")]
+[assembly: AssemblyKeyFile(@"..\tcs\bin\TopCoder.snk")]
 [assembly: AssemblyKeyName("")]
