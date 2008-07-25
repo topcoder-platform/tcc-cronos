b9b634a60fa017cef9ce901930d5b997e74246e9
amarcu <amarcu@fb370eea-3af6-4597-97f7-f7400a59c12a>
2008-07-25 22:05:58 +0000

    [TCCC-367] In AjaxBridge, method selectWinner, payPalOrderId can be an alphanumeric string
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@127167 fb370eea-3af6-4597-97f7-f7400a59c12a

--- widget_bridge/trunk/src/java/main/com/topcoder/widgets/bridge/AjaxBridgeServlet.java
+++ widget_bridge/trunk/src/java/main/com/topcoder/widgets/bridge/AjaxBridgeServlet.java
@@ -1016,9 +1016,6 @@ public class AjaxBridgeServlet extends HttpServlet {
                     if (checkLongIfLessThanZero(submissionId, "submissionId", response)) {
                         return;
                     }
-                    if (checkLongIfLessThanZero(payPalOrderId, "payPalOrderId", response)) {
-                        return;
-                    }
                     if (checkIntegerIfLessThanZero(submissionId, "place", response)) {
                         return;
                     }
