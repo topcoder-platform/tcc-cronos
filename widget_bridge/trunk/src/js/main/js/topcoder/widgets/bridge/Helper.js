2eba3750418573d4cc6ecb30238e56a9456972fc
isv <isv@fb370eea-3af6-4597-97f7-f7400a59c12a>
2008-08-15 23:04:40 +0000

    TCCC-415 - The special characters are properly escaped when converting objects to JSON
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@130239 fb370eea-3af6-4597-97f7-f7400a59c12a

--- widget_bridge/trunk/src/js/main/js/topcoder/widgets/bridge/Helper.js
+++ widget_bridge/trunk/src/js/main/js/topcoder/widgets/bridge/Helper.js
@@ -112,6 +112,6 @@ js.topcoder.widgets.bridge.Helper = {
         if (str == null) {
             return str;
         }
-        return str.replace(/"/g, "\\\"").replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\\/g, "\\\\").replace(/\t/g, "\\t");
+        return str.replace(/\\/g, "\\\\").replace(/"/g, "\\\"").replace(/\n/g, "\n").replace(/\r/g, "\r").replace(/\t/g, "\t");
     }
 }
