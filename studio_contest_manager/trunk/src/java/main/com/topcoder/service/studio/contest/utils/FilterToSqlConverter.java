91df8af19580295ffa2bc8a1b040efd61265966c
isv <isv@fb370eea-3af6-4597-97f7-f7400a59c12a>
2008-09-09 19:47:54 +0000

    TCCC-97 - Integrating Auto Pilot 1.0 component - Updated SQL_SEARCH_CONTEXT to include contest_status_lu table
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@133600 fb370eea-3af6-4597-97f7-f7400a59c12a

--- studio_contest_manager/trunk/src/java/main/com/topcoder/service/studio/contest/utils/FilterToSqlConverter.java
+++ studio_contest_manager/trunk/src/java/main/com/topcoder/service/studio/contest/utils/FilterToSqlConverter.java
@@ -58,8 +58,11 @@ public class FilterToSqlConverter {
     	+ "SELECT contest.contest_id,contest.contest_channel_id,contest.name,contest.contest_type_id, "  
         + "contest.project_id,contest.tc_direct_project_id,contest.contest_status_id,contest.contest_detailed_status_id,contest.forum_id, " 
         + "contest.event_id,contest.start_time,contest.end_time,contest.winner_announcement_time, "  
-        + "contest.create_user_id "  
-        + "FROM contest WHERE ";  
+        + "contest.create_user_id "
+        + "FROM contest "
+        + "INNER JOIN contest_status_lu ON contest.contest_status_id = contest_status_lu.contest_status_id "
+        + "INNER JOIN contest_channel_lu ON contest.contest_channel_id = contest_channel_lu.contest_channel_id "
+        + "WHERE ";
 
 
     /**
