90a2dfb7caee9bd6a26cca2f0b80fbacc295cb90
isv <isv@fb370eea-3af6-4597-97f7-f7400a59c12a>
2008-10-09 16:47:57 +0000

    Fixed the bug with locating all template fields (loops and conditions are also analyzed now)
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@138247 fb370eea-3af6-4597-97f7-f7400a59c12a

--- cockpit_phase_handlers/trunk/src/java/main/com/topcoder/clientcockpit/phases/messagegenerators/DefaultEmailMessageGenerator.java
+++ cockpit_phase_handlers/trunk/src/java/main/com/topcoder/clientcockpit/phases/messagegenerators/DefaultEmailMessageGenerator.java
@@ -17,6 +17,8 @@ import com.topcoder.util.file.Template;
 import com.topcoder.util.file.fieldconfig.Field;
 import com.topcoder.util.file.fieldconfig.Node;
 import com.topcoder.util.file.fieldconfig.TemplateFields;
+import com.topcoder.util.file.fieldconfig.Condition;
+import com.topcoder.util.file.fieldconfig.Loop;
 
 
 /**
@@ -69,39 +71,14 @@ public class DefaultEmailMessageGenerator implements EmailMessageGenerator, Seri
         ExceptionUtils.checkNull(phase, null, null, "Phase should not be null.");
         ExceptionUtils.checkNull(template, null, null, "Template should not be null.");
         try {
-            //The XsltTemplate.getFields() never returns null
             TemplateFields fields = template.getFields();
-            //The TemplateFields.getNodes() never returns null
             for (Node node : fields.getNodes()) {
                 if (node instanceof Field) {
-                    Field field = (Field) node;
-                    //Only apply the data if Field is not readOnly
-                    if (!field.isReadOnly()) {
-                        //Use field name as the key
-                        String key = field.getName();
-
-                        //Get the value of the node from phase.attributes
-                        Serializable value = phase.getAttribute(key);
-
-                        //If null was returned get the value of the node from phase.project.attributes
-                        if (value == null && phase.getProject() != null) {
-                            value = phase.getProject().getAttribute(key);
-                        }
-
-                        //Neither exist in phase nor project attributes
-                        if (value == null) {
-                            throw new EmailMessageGenerationException("Template data [" + key
-                                + "] does not exist in either the phase or project attributes.");
-                        }
-
-                        //Must be type of Long, Date or String
-                        if (value instanceof Number || value instanceof Date || value instanceof String) {
-                            field.setValue(value.toString());
-                        } else {
-                            throw new EmailMessageGenerationException("Template data [" + key
-                                + "] type is neither Long, Date nor String: " + value.getClass());
-                        }
-                    }
+                    process((Field) node, phase);
+                } else if (node instanceof Condition) {
+                    process((Condition) node, phase);
+                } else if (node instanceof Loop) {
+                    process((Loop) node, phase);
                 }
             }
             return DocumentGenerator.getInstance().applyTemplate(fields);
@@ -110,6 +87,108 @@ public class DefaultEmailMessageGenerator implements EmailMessageGenerator, Seri
                 : new EmailMessageGenerationException("Error while generating email message body.", e);
         } catch (ConfigManagerException e) {
             throw new EmailMessageGenerationException("Error while generating email message body.", e);
+        }
+    }
+
+    /**
+     * <p>Fills the specified template field with data taken from the contest corresponding to specified phase.</p>
+     *
+     * @param field a <code>Field</code> providing the template field which has to be filled with data from the
+     *        specified phase.
+     * @param phase a <code>Phase</code> referencing the contest and status which the message is generated for.
+     * @throws EmailMessageGenerationException If any error occurs while generating the message (also if some template
+     *         data does not exist in either the phase or project attributes, or if it is not <code>Long</code>,
+     *         <code>Date</code> or <code>String</code>).
+     */
+    private void process(Field field, Phase phase) throws EmailMessageGenerationException {
+        //Only apply the data if Field is not readOnly
+        if (!field.isReadOnly()) {
+            //Use field name as the key
+            String key = field.getName();
+            Serializable value = getValue(phase, key);
+
+            //Must be type of Long, Date or String
+            if (value instanceof Number || value instanceof Date || value instanceof String) {
+                field.setValue(value.toString());
+            } else {
+                throw new EmailMessageGenerationException("Template data [" + key
+                    + "] type is neither Long, Date nor String: " + value.getClass());
+            }
+        }
+    }
+
+    /**
+     * <p>Gets the value matching the specified key from the specified phase.</p>
+     *
+     * @param phase a <code>Phase</code> referencing the contest and status which the message is generated for.
+     * @param key a <code>String</code> providing the key to get the matching value for.
+     * @return a <code>Serializable</code> providing the value matching the specified key. 
+     * @throws EmailMessageGenerationException If any error occurs while generating the message (also if some template
+     *         data does not exist in either the phase or project attributes, or if it is not <code>Long</code>,
+     *         <code>Date</code> or <code>String</code>).
+     */
+    private Serializable getValue(Phase phase, String key) throws EmailMessageGenerationException {
+        //Get the value of the node from phase.attributes
+        //If null was returned get the value of the node from phase.project.attributes
+        Serializable value = phase.getAttribute(key);
+        if (value == null && phase.getProject() != null) {
+            value = phase.getProject().getAttribute(key);
+        }
+
+        //Neither exist in phase nor project attributes
+        if (value == null) {
+            throw new EmailMessageGenerationException("Template data [" + key
+                + "] does not exist in either the phase or project attributes.");
+        }
+        return value;
+    }
+
+    /**
+     * <p>Process the specified template condition by filling the fields included to condition with data taken from the
+     * contest corresponding to specified phase.</p>
+     *
+     * @param condition a <code>Condition</code> representing the condition in template which is to be processed.
+     * @param phase a <code>Phase</code> referencing the contest and status which the message is generated for.
+     * @throws EmailMessageGenerationException If any error occurs while generating the message (also if some template
+     *         data does not exist in either the phase or project attributes, or if it is not <code>Long</code>,
+     *         <code>Date</code> or <code>String</code>).
+     */
+    private void process(Condition condition, Phase phase) throws EmailMessageGenerationException {
+        condition.setValue(String.valueOf(getValue(phase, condition.getName())));
+        Node[] nodes = condition.getSubNodes().getNodes();
+        for (int i = 0; i < nodes.length; i++) {
+            Node node = nodes[i];
+            if (node instanceof Field) {
+                process((Field) node, phase);
+            } else if (node instanceof Condition) {
+                process((Condition) node, phase);
+            } else if (node instanceof Loop) {
+                process((Loop) node, phase);
+            }
+        }
+    }
+
+    /**
+     * <p>Process the specified template loop by filling the fields included to loop with data taken from the contest
+     * corresponding to specified phase.</p>
+     *
+     * @param loop a <code>Loop</code> representing the loop in template which is to be processed.
+     * @param phase a <code>Phase</code> referencing the contest and status which the message is generated for.
+     * @throws EmailMessageGenerationException If any error occurs while generating the message (also if some template
+     *         data does not exist in either the phase or project attributes, or if it is not <code>Long</code>,
+     *         <code>Date</code> or <code>String</code>).
+     */
+    private void process(Loop loop, Phase phase) throws EmailMessageGenerationException {
+        Node[] nodes = loop.getSampleLoopItem().getNodes();
+        for (int i = 0; i < nodes.length; i++) {
+            Node node = nodes[i];
+            if (node instanceof Field) {
+                process((Field) node, phase);
+            } else if (node instanceof Condition) {
+                process((Condition) node, phase);
+            } else if (node instanceof Loop) {
+                process((Loop) node, phase);
+            }
         }
     }
 }
