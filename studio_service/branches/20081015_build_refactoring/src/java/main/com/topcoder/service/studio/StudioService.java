176dc871d5b613519e4342da09d312f7f35f3ce3
dbelfer <dbelfer@fb370eea-3af6-4597-97f7-f7400a59c12a>
2008-10-15 16:52:22 +0000

    new branch
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@138820 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ studio_service/branches/20081015_build_refactoring/src/java/main/com/topcoder/service/studio/StudioService.java
@@ -0,0 +1,572 @@
+/*
+ * Copyright (C) 2008 TopCoder Inc., All Rights Reserved.
+ */
+package com.topcoder.service.studio;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import com.topcoder.search.builder.filter.Filter;
+import com.topcoder.service.studio.contest.ContestChangeHistory;
+import com.topcoder.service.studio.contest.ContestManagementException;
+import com.topcoder.service.studio.contest.ContestPayment;
+import com.topcoder.service.studio.contest.EntityAlreadyExistsException;
+import com.topcoder.service.studio.contest.EntityNotFoundException;
+
+/**
+ * <p>
+ * This is service interface which defines operations available for client. It
+ * allows providing different operations under contest and submission. It allows
+ * getting, updating, and creating contest data; get, remove and update
+ * submission data; get some additional information like content's categories,
+ * statuses and file types.
+ * </p>
+ * <p>
+ * This interface defines the webservices endpoint with the operation. It uses
+ * the annotations to defined the requests and responses definitions.
+ * </p>
+ * 
+ * <p>
+ * It has the following annotations: "@WebService". All the other annotations
+ * (also in the methods) are optional: WebParam, WebMethod are not necessary
+ * because if WebMethod is not used then automatically all methods are
+ * WebMethod.
+ * </p>
+ * 
+ * @author fabrizyo, TCSDEVELOPER
+ * @version 1.0
+ */
+// @WebService
+@Remote
+public interface StudioService {
+	/**
+	 * Create contest for project. Return contest populated with id
+	 * 
+	 * @param contestData
+	 *            the contestData used to create the Contest
+	 * @param tcDirectProjectId
+	 *            the tc project id set to Contest
+	 * @return the ContestData persisted and polulated with the new id
+	 * @throws IllegalArgumentWSException
+	 *             if the contestData is null, if the tcDirectProjectId is less
+	 *             than 0
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 */
+	public ContestData createContest(ContestData contestData,
+			long tcDirectProjectId) throws PersistenceException;
+
+	/**
+	 * <p>
+	 * Get contest by id
+	 * 
+	 * @param contestId
+	 *            the id used to retrireve the contest
+	 * @return the ContestData retrieved
+	 * @throws IllegalArgumentWSException
+	 *             if the contestId is less than 0
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 * @throws ContestNotFoundException
+	 *             if the contest is not found
+	 */
+	public ContestData getContest(long contestId) throws PersistenceException,
+			ContestNotFoundException;
+
+	/**
+	 * <p>
+	 * Get contests for given project. Return an empty list if there are no
+	 * contests.
+	 * 
+	 * @param tcDirectProjectId
+	 *            the tc Direct Projec tId used to retrieve the ContestData
+	 * @return the contest datas which represents the contests
+	 * @throws IllegalArgumentWSException
+	 *             if the tcDirectProjectId is less than 0
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 * @throws ProjectNotFoundException
+	 *             if the project is not found
+	 */
+	public List<ContestData> getContestsForProject(long tcDirectProjectId)
+			throws PersistenceException, ProjectNotFoundException;
+
+	/**
+	 * <p>
+	 * Update contest
+	 * 
+	 * @param contestData
+	 *            the contest data to update
+	 * @throws IllegalArgumentWSException
+	 *             if the argument is null
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 * @throws ContestNotFoundException
+	 *             if the contest is not found
+	 */
+	public void updateContest(ContestData contestData)
+			throws PersistenceException, ContestNotFoundException;
+
+	/**
+	 * <p>
+	 * Update contest status
+	 * 
+	 * @param contestId
+	 *            the id of contest to what the status will be updated
+	 * @param newStatusId
+	 *            the id of the new status
+	 * @throws IllegalArgumentWSException
+	 *             if any id is less than 0
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 * @throws StatusNotAllowedException
+	 *             if the status is not allowed
+	 * @throws StatusNotFoundException
+	 *             if the status is not found
+	 * @throws ContestNotFoundException
+	 *             if the contest is not found
+	 */
+	public void updateContestStatus(long contestId, long newStatusId)
+			throws PersistenceException, StatusNotAllowedException,
+			StatusNotFoundException, ContestNotFoundException;
+
+	/**
+	 * <p>
+	 * Upload document to contest. Return document populated with id.
+	 * 
+	 * @param uploadedDocument
+	 *            the uploadDocument to update
+	 * @return the same instance passed in argument with the documentId updated
+	 * @throws IllegalArgumentWSException
+	 *             if the argument is null
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 * @throws ContestNotFoundException
+	 *             if the contest is not found
+	 */
+	public UploadedDocument uploadDocumentForContest(
+			UploadedDocument uploadedDocument) throws PersistenceException,
+			ContestNotFoundException;
+
+	/**
+	 * <p>
+	 * Upload document. Return document populated with id.
+	 * 
+	 * @param uploadedDocument
+	 *            the uploadDocument to update
+	 * @return the same instance passed in argument with the documentId updated
+	 * @throws IllegalArgumentWSException
+	 *             if the argument is null
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 */
+	public UploadedDocument uploadDocument(UploadedDocument data)
+			throws PersistenceException;
+
+	/**
+	 * <p>
+	 * Links a document to a contest.
+	 * 
+	 * @param documentId
+	 *            document id, which document will be linked.
+	 * @param contestId
+	 *            cpntest id, which contest will be linked.
+	 * @throws IllegalArgumentWSException
+	 *             if any id is less than 0
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 * @throws ContestNotFoundException
+	 *             if the contest or document is not found.
+	 */
+	public void addDocumentToContest(long documentId, long contestId)
+			throws PersistenceException, ContestNotFoundException;
+
+	/**
+	 * <p>
+	 * Remove document from contest.
+	 * 
+	 * @param document
+	 *            the document to remove
+	 * @throws IllegalArgumentWSException
+	 *             if the argument is null
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 * @throws DocumentNotFoundException
+	 *             if the document is not found
+	 */
+	public void removeDocumentFromContest(UploadedDocument document)
+			throws PersistenceException, DocumentNotFoundException;
+
+	/**
+	 * <p>
+	 * Retrieve submission data. return an empty list if there are no submission
+	 * data
+	 * 
+	 * @param contestId
+	 *            the contest if used
+	 * @return the submission data of contest
+	 * @throws IllegalArgumentWSException
+	 *             if the argument id less than 0
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 * @throws ContestNotFoundException
+	 *             if the contest is not found
+	 */
+	public List<SubmissionData> retrieveSubmissionsForContest(long contestId)
+			throws PersistenceException, ContestNotFoundException;
+
+	/**
+	 * <p>
+	 * Retrieve submission by member. return an empty list if there are no
+	 * submission data
+	 * 
+	 * @param userId
+	 *            the user id used to retrieve the submissions
+	 * @return the submissions by member
+	 * @throws IllegalArgumentWSException
+	 *             if the id is less than 0
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 */
+	public List<SubmissionData> retrieveAllSubmissionsByMember(long userId)
+			throws PersistenceException, UserNotAuthorizedException;
+
+	/**
+	 * <p>
+	 * Update submission.
+	 * 
+	 * @param submission
+	 *            the submission to update
+	 * @throws IllegalArgumentWSException
+	 *             if the submission is null
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 */
+	public void updateSubmission(SubmissionData submission)
+			throws PersistenceException;
+
+	/**
+	 * <p>
+	 * Remove submission
+	 * 
+	 * @param submissionId
+	 *            the id of submission to remove
+	 * @throws IllegalArgumentWSException
+	 *             if the submissionId is less than 0
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 */
+	public void removeSubmission(long submissionId) throws PersistenceException;
+
+	/**
+	 * <p>
+	 * Get contest statuses. Return an empty list if there are no
+	 * ContestStatusData
+	 * 
+	 * @return the list of status
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 */
+	public List<ContestStatusData> getStatusList() throws PersistenceException;
+
+	/**
+	 * <p>
+	 * Get submission types
+	 * 
+	 * @return the file types
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 * @throws UserNotAuthorizedException
+	 *             if the user is not authorized to perform this method
+	 */
+	public String getSubmissionFileTypes() throws PersistenceException;
+
+	/**
+	 * <p>
+	 * This is going to fetch all the currently available contests.
+	 * </p>
+	 * 
+	 * @return the list of all available contents (or empty if none found)
+	 * 
+	 * @throws PersistenceException
+	 *             if any error occurs when getting contest.
+	 */
+	public List<ContestData> getAllContests() throws PersistenceException;
+
+	/**
+	 * <p>
+	 * This is going to get all the matching contest entities that fulfill the
+	 * input criteria.
+	 * </p>
+	 * 
+	 * @param filter
+	 *            a search filter used as criteria for contests.
+	 * @return a list (possibly empty) of all the matched contest entities.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if the input filter is null or filter is not supported for
+	 *             searching
+	 * @throws PersistenceException
+	 *             if any error occurs when getting contest.
+	 */
+	public List<ContestData> searchContests(Filter filter)
+			throws PersistenceException;
+
+	/**
+	 * Returns all contests for a particular client ID, provided as a long
+	 * 
+	 * @param clientId
+	 *            client Id to search for.
+	 * @return all contests for the given client ID.
+	 * @throws PersistenceException
+	 *             if any error occurs when getting contest.
+	 */
+	public List<ContestData> getContestsForClient(long clientId)
+			throws PersistenceException;
+
+	/**
+	 * <p>
+	 * Gets the submission with the given id.
+	 * </p>
+	 * 
+	 * @param submissionId
+	 *            The id of the submission to get
+	 * @return the submission with the given id, or null if not found
+	 * @throws PersistenceException
+	 *             If any error occurs during the retrieval
+	 */
+	public SubmissionData retrieveSubmission(long submissionId)
+			throws PersistenceException;
+
+	/**
+	 * <p>
+	 * This is going to fetch all the currently available contest types.
+	 * </p>
+	 * 
+	 * @return the list of all available content types (or empty if none found)
+	 * 
+	 * @throws PersistenceException
+	 *             if any error occurs when getting contest.
+	 */
+	public List<ContestTypeData> getAllContestTypes()
+			throws PersistenceException;
+
+	/**
+	 * <p>
+	 * Remove document
+	 * 
+	 * @param documentId
+	 *            the id of document to remove
+	 * @throws IllegalArgumentWSException
+	 *             if the documentId is less than 0
+	 * @throws PersistenceException
+	 *             if some persistence errors occur
+	 */
+	public boolean removeDocument(long documentId) throws PersistenceException;
+
+	/**
+	 * <p>
+	 * Get matched the MimeType id.
+	 * </p>
+	 * 
+	 * @param ContentType.
+	 * @return the matched MimeType id.
+	 * 
+	 * @throws PersistenceException
+	 *             if any error occurs when getting MimeType
+	 * @throws IllegalArgumentWSException
+	 *             if the argument is null or empty.
+	 */
+	public long getMimeTypeId(String contentType) throws PersistenceException;
+
+	/**
+	 * <p>
+	 * Purchase submission.
+	 * </p>
+	 * <p>
+	 * set the price of submission. create an entry at submission payment table
+	 * </p>
+	 * 
+	 * @param submissionId
+	 *            the id of submission to remove
+     * @param payPalOrderId PayPal order id.
+     * @param securityToken the security token.
+     *
+	 * @throws PersistenceException
+	 *             if any error occurs when purchasing submission.
+	 * @throws IllegalArgumentWSException
+	 *             if the submissionId is less than 0 or price is negative.
+	 */
+	public void purchaseSubmission(long submissionId, String payPalOrderId, String securityToken)
+			throws PersistenceException;
+
+    /**
+     * <p>
+     * Creates a new contest payment and returns the created contest payment.
+     * </p>
+     *
+     * @param contestPayment the contest payment to create.
+     * @param securityToken the security token.
+     * 
+     * @return the created contest payment.
+     *
+     * @throws IllegalArgumentException if the arg is null.
+     * @throws EntityAlreadyExistsException if the entity already exists in the persistence.
+     * @throws ContestManagementException if any other error occurs.
+     */
+    public ContestPaymentData createContestPayment(ContestPaymentData contestPayment,String securityToken) throws PersistenceException;
+    
+    /**
+     * <p>
+     * Gets contest payment by id, and return the retrieved contest payment. If the contest payment doesn't exist, null is returned.
+     * </p>
+     *
+     * @param contestPaymentId the contest payment id
+     * @return the retrieved contest, or null if id doesn't exist
+     *
+     * @throws ContestManagementException if any error occurs when getting contest.
+     */
+    public ContestPaymentData getContestPayment(long contestPaymentId) throws PersistenceException;
+
+    /**
+     * <p>
+     * Updates contest payment data.
+     * </p>
+     *
+     * @param contestPayment the contest payment to update
+     * @throws IllegalArgumentException if the argument is null.
+     * @throws EntityNotFoundException if the contest payment doesn't exist in persistence.
+     * @throws ContestManagementException if any error occurs when updating contest payment.
+     */
+    public void editContestPayment(ContestPaymentData contestPayment) throws PersistenceException;
+
+    /**
+     * <p>
+     * Removes contest payment, return true if the contest payment exists and removed successfully, return false if it
+     * doesn't exist.
+     * </p>
+     *
+     * @param contestPaymentId the contest payment id
+     * @return true if the contest payment exists and removed successfully, return false if it doesn't exist
+     * @throws ContestManagementException if any error occurs.
+     */
+    public boolean removeContestPayment(long contestPaymentId) throws PersistenceException;
+    
+
+    /**
+     * <p>
+     * This is going to fetch all the currently available media.
+     * </p>
+     * 
+     * @return the list of all available mediums (or empty if none found)
+     * 
+     * @throws PersistenceException
+     *             if any error occurs when getting medium.
+     */
+    public List<MediumData> getAllMediums() throws PersistenceException;
+    
+    /**
+     * Set submission placement.
+     * 
+     * @param submissionId Submission Id.
+     * @param placement placement
+     * @throws PersistenceException if any error occurs when setting placement.
+     * @since TCCC-353
+     */
+    public void setSubmissionPlacement(long submissionId,int placement)throws PersistenceException;
+    
+    /**
+     * Marks submission for purchase.
+     * 
+     * @param submissionId Submission Id.
+     * @throws PersistenceException if any error occurs when marking for purchase. 
+     * 
+     *  @since TCCC-353
+     */
+    public void markForPurchase(long submissionId)throws PersistenceException;
+    
+    /**
+     * Add a change history entity.
+     * 
+     * @param history
+     *            Change history entity to be added.
+     * 
+     * @throws PersistenceException
+     *             if any other error occurs.
+     */
+    public void addChangeHistory(List<ChangeHistoryData> history) throws PersistenceException;
+
+    /**
+     * Returns change history entity list.
+     * 
+     * @param contestId
+     *            contest id to search for.
+     * @return Change history entities match the contest id.
+     * @throws PersistenceException
+     *             if any other error occurs.
+     */
+    public List<ChangeHistoryData> getChangeHistory(long contestId) throws PersistenceException;
+
+    /**
+     * Returns latest change history entity list.
+     * 
+     * @param contestId
+     *            contest id to search for.
+     * 
+     * @return Latest change history entities match the contest id and transaction id.
+     * @throws PersistenceException
+     *             if any other error occurs.
+     */
+    public List<ChangeHistoryData> getLatestChanges(long contestId) throws PersistenceException;
+
+    /**
+     * Delete contest.
+     * 
+     * @param contestId
+     *            contest id to delete.
+     * @throws PersistenceException
+     *             if any other error occurs.
+     */
+    public void deleteContest(long contestId) throws PersistenceException;
+
+    /**
+     * <p>
+     * This is going to fetch all the currently available contests. This method only return values used in my project widget.
+     * </p>
+     * 
+     * @return the list of all available contents (or empty if none found)
+     * 
+     * @throws PersistenceException
+     *             if any error occurs when getting contest.
+     */
+    public List<ContestData> getAllContestHeaders() throws PersistenceException;
+}
---
+++ studio_service/branches/20081015_build_refactoring/src/java/tests/com/topcoder/service/studio/Demo.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2008 TopCoder Inc., All Rights Reserved.
+ */
+package com.topcoder.service.studio;
+
+import com.topcoder.service.studio.ejb.StudioServiceBeanTest;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.datatype.DatatypeFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.GregorianCalendar;
+
+/**
+ * This is demo test to demonstrate basic functionality of the component.
+ *
+ * @author fabrizyo, TCSDEVELOPER
+ * @version 1.0
+ */
+public class Demo extends TestCase {
+    /**
+     * <p>Returns the test suite of this class.</p>
+     *
+     * @return the test suite of this class.
+     */
+    public static Test suite() {
+        return new TestSuite(Demo.class);
+    }
+
+    /**
+     * <p>Sets up test environment.</p>
+     *
+     * @throws Exception to junit
+     */
+    protected void setUp() throws Exception {
+    }
+
+    /**
+     * <p>Tears down test environment.</p>
+     *
+     * @throws Exception to junit
+     */
+    protected void tearDown() throws Exception {
+    }
+
+    /**
+     * Some basic functionality for Studio Service.
+     *
+     * @throws Exception when it occurs deeper
+     */
+    public void test() throws Exception {
+        // 'set up' studio service
+        StudioServiceBeanTest test = new StudioServiceBeanTest();
+        test.setUp();
+        StudioService studioService = test.target;
+
+        //Create the competition and set its values
+        ContestData contestData = new ContestData();
+        contestData.setName("contestName");
+        contestData.setContestId(20);
+
+        //Create the upload documentation
+        List<UploadedDocument> documentationUpload = new ArrayList<UploadedDocument>();
+        UploadedDocument document = new UploadedDocument();
+        document.setContestId(20);
+        document.setDescription("Design Studio Distribution for Kink DAO");
+        document.setFile("designDistribution".getBytes());
+        documentationUpload.add(document);
+
+        //add the design distribution documentation to competition
+
+        //launch the competition now
+        // create the timestamp with the current date
+        GregorianCalendar cal = new GregorianCalendar();
+        // create the XMLGregorianCalendar
+        XMLGregorianCalendar xmlCal = DatatypeFactory.newInstance().newXMLGregorianCalendar(cal);
+
+        //create the competition associated with a tc project
+        studioService.createContest(contestData, 40);
+        //now the competition is created and persisted
+
+        //Get this competition back
+        contestData = studioService.getContest(20);
+        //the competition is retrieved
+        //You can get the previous values through the getters, the getters are the same as the setters
+
+        //get the previous competition which is related with the project with id=40
+        List<ContestData> contestDatas = studioService.getContestsForProject(40);
+        //the competition is retrieved (only 1 item)
+
+        //get the file types
+        String fileTypes = studioService.getSubmissionFileTypes();
+        //it now contains "pdf","rtf","jpg","png",etc... separated by commas
+
+        //remove the previous created document from the related competition
+        //the design distribution is removed now
+    }
+}
