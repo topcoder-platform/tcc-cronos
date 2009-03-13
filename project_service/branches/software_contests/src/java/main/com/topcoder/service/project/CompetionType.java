2508ce02ce91f36db5953cb78b4f12260426c7e9
sma <sma@fb370eea-3af6-4597-97f7-f7400a59c12a>
2009-03-13 02:47:29 +0000

    branched to add software contests.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@150779 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ project_service/branches/software_contests/src/java/main/com/topcoder/service/project/CompetionType.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2008 TopCoder Inc., All Rights Reserved.
+ */
+package com.topcoder.service.project;
+
+/**
+ * <p>
+ * Represents the competition type.
+ * </p>
+ * 
+ * <p>
+ * TopCoder Service Layer Integration 3 Assembly change: new competition types are added.
+ * </p>
+ *
+ * @author FireIce
+ * @version 1.0
+ *
+ */
+public enum CompetionType {
+    STUDIO
+}
