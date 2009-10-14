76cb72110946263c9ca8e1ede83f4860e1ed38e3
sma <sma@fb370eea-3af6-4597-97f7-f7400a59c12a>
2009-10-14 13:35:24 +0000

    Updated for getCommonProjectContestData transaction to not_supported.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@167038 fb370eea-3af6-4597-97f7-f7400a59c12a

--- pipeline_service_facade/trunk/src/java/main/com/topcoder/service/pipeline/ejb/PipelineServiceFacadeBean.java
+++ pipeline_service_facade/trunk/src/java/main/com/topcoder/service/pipeline/ejb/PipelineServiceFacadeBean.java
@@ -310,6 +310,7 @@ public class PipelineServiceFacadeBean implements PipelineServiceFacadeRemote, P
      *             if error during retrieval from database.
      * @since 1.0.1
      */
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
     public List<CommonPipelineData> getCommonPipelineData(Date startDate, Date endDate, boolean overdueContests)
             throws ContestPipelineServiceException {
         logger.log(Level.DEBUG, "Enter getCommonPipelineData(Date startDate, Date endDate, boolean overdueContests method.");
