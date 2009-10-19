67f5f73e286efea4f26b8e30214ce33f6a8e3430
sma <sma@fb370eea-3af6-4597-97f7-f7400a59c12a>
2009-10-19 18:41:21 +0000

    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@167449 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ contest_service_facade/branches/release11/src/java/tests/com/topcoder/service/facade/contest/AllTests.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2008, TopCoder, Inc. All rights reserved
+ */
+package com.topcoder.service.facade.contest;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import com.topcoder.service.facade.contest.failuretests.FailureTests;
+
+
+/**
+ * <p>This test case aggregates all test cases.</p>
+ *
+ * @author TopCoder
+ * @version 1.0
+ */
+public class AllTests extends TestCase {
+    public static Test suite() {
+        final TestSuite suite = new TestSuite();
+
+        //unit tests
+        suite.addTest(UnitTests.suite());
+        suite.addTest(FailureTests.suite());
+        //suite.addTest(AccuracyTests.suite());
+        //suite.addTest(StressTests.suite());
+
+        return suite;
+    }
+}
