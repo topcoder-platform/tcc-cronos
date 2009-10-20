2fb0d44267b1c09d0cca82381a59ee5d8a41089d
sma <sma@fb370eea-3af6-4597-97f7-f7400a59c12a>
2009-10-20 18:56:50 +0000

    updated persistence.jar
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@167542 fb370eea-3af6-4597-97f7-f7400a59c12a

--- contest_eligibility_validation/trunk/test_files/table.sql
+++ contest_eligibility_validation/trunk/test_files/table.sql
@@ -7,28 +7,19 @@ create table 'informix'.user_group_xref (
     create_date DATETIME YEAR TO FRACTION default CURRENT YEAR TO FRACTION
 );
 
-CREATE SEQUENCE CONTEST_ELIGIBILITY_SEQ INCREMENT BY 1 START WITH 1 MINVALUE 1;
-
 create table 'informix'.contest_eligibility (
-    id INT8 not null,
-    contest_id INT8,
-    is_studio SMALLINT
-);
+contest_eligibility_id DECIMAL(10, 0) not null,
+contest_id DECIMAL(10,0) not null,
+is_studio SMALLINT NOT NULL,
+PRIMARY KEY (contest_eligibility_id) constraint contest_eligibility_pk
+)
 
-alter table 'informix'.contest_eligibility add constraint primary key 
-	(id)
-	constraint contest_eligibility_pk;
-	
 create table 'informix'.group_contest_eligibility (
-    id INT8 not null,
-    group_id INT8
-);
-alter table 'informix'.group_contest_eligibility add constraint primary key 
-	(id)
-	constraint group_contest_eligibility_pk;
+contest_eligibility_id DECIMAL(10, 0) not null,
+group_id DECIMAL(10,0) not null,
+PRIMARY KEY (contest_eligibility_id) constraint group_contest_eligibility_pk,
+FOREIGN KEY (contest_eligibility_id) REFERENCES contest_eligibility(contest_eligibility_id) constraint contest_eligibility_fk
+)
 
-alter table 'informix'.group_contest_eligibility add constraint foreign key 
-	(id)
-	references 'informix'.contest_eligibility
-	(id) 
-	constraint  contest_eligibility_fk;
+
+CREATE SEQUENCE CONTEST_ELIGIBILITY_SEQ INCREMENT BY 1 START WITH 1 MINVALUE 1;
