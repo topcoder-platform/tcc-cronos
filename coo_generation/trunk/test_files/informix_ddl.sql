7b37192f84b736f9e3d24284aa55ae11a7588c3e
sma <sma@fb370eea-3af6-4597-97f7-f7400a59c12a>
2010-02-15 23:36:44 +0000

    BUGR-3212
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@172906 fb370eea-3af6-4597-97f7-f7400a59c12a

--- coo_generation/trunk/test_files/informix_ddl.sql
+++ coo_generation/trunk/test_files/informix_ddl.sql
@@ -1,46 +1,22 @@
 
 
-create table third_party_library(
+create table 'informix'.third_party_library(
+third_party_library_id DECIMAL(10,0) not null,
 name varchar(50),
+description varchar(100),
 version varchar(50),
 url varchar(50),
 license varchar(50),
-usage_comments varchar(50),
-path varchar(50),
+usage_comments varchar(150),
+path varchar(200),
 alias varchar(50),
-notes varchar(50),
+notes varchar(150),
 category varchar(50)
 );
 
 
-create table project(
-project_id int,
-component_name varchar(50),
-version_id varchar(50),
-category varchar(50),
-phase_desc varchar(50),
-end_date DATETIME YEAR TO FRACTION default CURRENT YEAR TO FRACTION
-);
-
-
-create table project_result(
-user_id varchar(50),
-project_id int,
-submit_ind int
-);
-
-
-create table submission_review(
-reviewer_id varchar(50),
-project_id int
-);
-
-
-
-create table project_info(
-project_info_type_id int,
-project_id int,
-value varchar(50)
-);
+alter table 'informix'.third_party_library add constraint primary key 
+	(third_party_library_id)
+	constraint pk_third_party_library;
 
 
