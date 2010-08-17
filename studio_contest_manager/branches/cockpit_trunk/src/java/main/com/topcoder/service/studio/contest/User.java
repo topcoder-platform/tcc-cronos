c9cf739f4e0c23014d791630117bf7672f9f2637
mashannon168 <mashannon168@fb370eea-3af6-4597-97f7-f7400a59c12a>
2010-08-17 20:15:27 +0000

    make User Serializable.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@186368 fb370eea-3af6-4597-97f7-f7400a59c12a

--- studio_contest_manager/branches/cockpit_trunk/src/java/main/com/topcoder/service/studio/contest/User.java
+++ studio_contest_manager/branches/cockpit_trunk/src/java/main/com/topcoder/service/studio/contest/User.java
@@ -1,11 +1,28 @@
+/*
+ * Copyright (C) 2008-2010 TopCoder Inc., All Rights Reserved.
+ */
 package com.topcoder.service.studio.contest;
 
-public class User {
+import java.io.Serializable;
+
+/**
+ * <p>An entity class representing a single user.</p>
+ *
+ * <p>
+ * Version 1.1 (Direct Permissions Setting Back-end and Integration Assembly 1.0) Change notes:
+ *   <ol>
+ *     <li>Marked the class as serializable.</li>
+ *   </ol>
+ * </p>
+
+ * @version 1.1
+ */
+public class User implements Serializable {
 
 	/**
 	 * Generated serial version id.
 	 */
-	private static final long serialVersionUID = -6993488651979864257L;
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Represents the contest id.
@@ -17,18 +34,38 @@ public class User {
 	 */
 	private String handle;
 
+    /**
+     * <p>Gets the user handle.</p>
+     *
+     * @return a <code>String</code> providing the user handle.
+     */
 	public String getHandle() {
 		return handle;
 	}
 
+    /**
+     * <p>Sets the user handle.</p>
+     *
+     * @param handle a <code>String</code> providing the user handle.
+     */
 	public void setHandle(String handle) {
 		this.handle = handle;
 	}
 
+    /**
+     * <p>Gets the user ID.</p>
+     *
+     * @return a <code>Long</code> providing the user ID.
+     */
 	public Long getUserId() {
 		return userId;
 	}
 
+    /**
+     * <p>Sets the user ID.</p>
+     *
+     * @param userId a <code>Long</code> providing the user ID.
+     */
 	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
