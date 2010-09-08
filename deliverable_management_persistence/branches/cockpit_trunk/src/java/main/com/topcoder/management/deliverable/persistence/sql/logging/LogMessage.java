317c24c395a70fd2ab06f8b021fc09b3ae8d105c
mashannon168 <mashannon168@fb370eea-3af6-4597-97f7-f7400a59c12a>
2010-09-08 19:50:15 +0000

    merged from spec_review3 (from trunk).
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@188188 fb370eea-3af6-4597-97f7-f7400a59c12a

--- deliverable_management_persistence/branches/cockpit_trunk/src/java/main/com/topcoder/management/deliverable/persistence/sql/logging/LogMessage.java
+++ deliverable_management_persistence/branches/cockpit_trunk/src/java/main/com/topcoder/management/deliverable/persistence/sql/logging/LogMessage.java
@@ -1,105 +1,140 @@
 /*
- * Copyright (C) 2007 TopCoder Inc., All Rights Reserved.
+ * Copyright (C) 2007-2010 TopCoder Inc., All Rights Reserved.
  */
 package com.topcoder.management.deliverable.persistence.sql.logging;
+
+import com.topcoder.management.deliverable.persistence.sql.Helper;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-
 /**
- * Encapsulates the entry log data and generates consistent log messages.
+ * <p>Encapsulates the entry log data and generates consistent log messages.</p>
+ *
+ * <p>Changes in 1.1: All attributes except for logMessage became final.</p>
+ *
+ * <p><strong>Thread Safety:</strong> This class is immutable so it's thread safe.</p>
+ *
+ * @author aubergineanode, saarixx, urtks, TCSDEVELOPER
+ * @version 1.1
+ * @since 1.0
  */
 public class LogMessage {
-    /** id for the log message */
-    private Long id;
 
-    /** Operator doing the action */
-    private String operator;
+    /**
+     * <p> The ID for the log message. Is initialized in the constructor and never changed after that. Can be any value.
+     * </p>
+     *
+     * <p>Changes in 1.1: Made final.</p>
+     */
+    private final Long id;
 
-    /** type of the action. */
-    private String type;
+    /**
+     * <p> The operator performing the action. Is initialized in the constructor and never changed after that. Can be
+     * any value. </p>
+     *
+     * <p>Changes in 1.1: Made final.</p>
+     */
+    private final String operator;
 
-    /** free text message to log */
-    private String message;
+    /**
+     * <p> The type of the action. Is initialized in the constructor and never changed after that. Can be any value.
+     * </p>
+     *
+     * <p>Changes in 1.1: Made final.</p>
+     */
+    private final String type;
 
-    /** exception to append to the log message */
-    private Throwable error;
+    /**
+     * <p> The text message to be logged. Is initialized in the constructor and never changed after that. Can be any
+     * value. </p>
+     *
+     * <p>Changes in 1.1: Made final.</p>
+     */
+    private final String message;
 
-    /** generated log message */
+    /**
+     * <p> The exception to be appended to the log message. Is initialized in the constructor and never changed after
+     * that. Can be any value. </p>
+     *
+     * <p>Changes in 1.1: Made final.</p>
+     */
+    private final Throwable error;
+
+    /**
+     * <p> The generated log message log message. Is initialized with not null value in getLogMessage() and never
+     * changed after that. </p>
+     */
     private String logMessage = null;
 
     /**
-     * Creates a log message. Any parameter can be null.
+     * <p>Creates a log message. Any parameter can be null.</p>
      *
-     * @param type the operator to log.
-     * @param id the project id to log.
-     * @param operator DOCUMENT ME!
-     * @param message a free text message.
-     * @param error an exception to append to the log message.
+     * @param type     the operator to log.
+     * @param id       the project id to log.
+     * @param operator the operator performing the action.
+     * @param message  a free text message.
+     * @param error    an exception to append to the log message.
      */
     public LogMessage(String type, Long id, String operator, String message, Throwable error) {
+
         this.id = id;
-
         this.type = type;
-
         this.operator = operator;
-
         this.message = message;
-
         this.error = error;
     }
 
     /**
-     * Creates a log message. Any parameter can be null
+     * <p>Creates a log message. Any parameter can be null.</p>
      *
-     * @param type the operator to log.
-     * @param id the project id to log.
-     * @param operator DOCUMENT ME!
-     * @param message a free text message.
+     * @param type     the operator to log.
+     * @param id       the project id to log.
+     * @param operator the operator performing the action.
+     * @param message  a free text message.
      */
     public LogMessage(String type, Long id, String operator, String message) {
         this(type, id, operator, message, null);
     }
 
     /**
-     * Return the error cause.Can be null.
+     * <p>Retrieves the ID for the log message.</p>
      *
-     * @return the error cause.
-     */
-    public Throwable getError() {
-        return error;
-    }
-
-    /**
-     * Returns the message.
-     *
-     * @return message
-     */
-    public String getMessage() {
-        return message;
-    }
-
-    /**
-     * Returns the type. Can be null.
-     *
-     * @return the operator
-     */
-    public String getType() {
-        return type;
-    }
-
-    /**
-     * Returns the id, can be null.
-     *
-     * @return id
+     * @return the ID for the log message
      */
     public Long getId() {
         return id;
     }
 
     /**
-     * Generate the message need to be logged for this class.
+     * <p>Retrieves the type of the action.</p>
+     *
+     * @return the type of the action
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * <p>Retrieves the text message to be logged.</p>
+     *
+     * @return the text message to be logged
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * <p>Retrieves the exception to be appended to the log message.</p>
+     *
+     * @return the exception to be appended to the log message
+     */
+    public Throwable getError() {
+        return error;
+    }
+
+    /**
+     * <p>Generates the message need to be logged for this class.</p>
      *
      * @return The log message
      */
@@ -108,12 +143,12 @@ public class LogMessage {
             StringBuffer buffer = new StringBuffer();
 
             buffer.append("type: ").append((type == null) ? "Unknown" : type).append(" id: ")
-                  .append((id == null) ? "Unknown" : id.toString()).append(" operator:")
-                  .append((operator == null) ? "Unknown" : operator).append(" - ").append(message);
+                    .append((id == null) ? "Unknown" : id.toString()).append(" operator:")
+                    .append((operator == null) ? "Unknown" : operator).append(" - ").append(message);
 
             //This should be done while the Logging Wrapper 1.2 is used.
 
-            //When the LW 1.3 would be ready, it will be possible pass the exception directly to LW.   
+            //When the LW 1.3 would be ready, it will be possible pass the exception directly to LW.
             if (error != null) {
                 buffer.append('\n').append(getExceptionStackTrace(error));
             }
@@ -125,13 +160,19 @@ public class LogMessage {
     }
 
     /**
-     * Return the exception stack trace string.
+     * <p>Returns the exception stack trace string.</p>
+     *
+     * <p>Changes in 1.1: Parameter checking - IllegalArgumentException is thrown in case the cause is null.</p>
      *
      * @param cause the exception to be recorded
      *
-     * @return stack strace
+     * @return stack the exception stack trace string
+     *
+     * @throws IllegalArgumentException if cause is null
      */
     public static String getExceptionStackTrace(Throwable cause) {
+        Helper.assertObjectNotNull(cause, "cause");
+
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
         cause.printStackTrace(new PrintStream(out));
@@ -140,9 +181,9 @@ public class LogMessage {
     }
 
     /**
-     * Override the toString method returns the log message.
+     * <p>Overrides the toString method: returns the log message.</p>
      *
-     * @return logged message
+     * @return the log message
      */
     public String toString() {
         return getLogMessage();
