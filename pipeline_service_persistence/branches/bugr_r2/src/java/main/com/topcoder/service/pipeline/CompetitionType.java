b1c06e532d5cd1b7c1bea82720ec278b49b42aac
mashannon168 <mashannon168@fb370eea-3af6-4597-97f7-f7400a59c12a>
2010-09-26 19:14:06 +0000

    created a branch as bugr_r2
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@189347 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ pipeline_service_persistence/branches/bugr_r2/src/java/main/com/topcoder/service/pipeline/CompetitionType.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2009 TopCoder Inc., All Rights Reserved.
+ */
+package com.topcoder.service.pipeline;
+
+/**
+ * The competition types supported in the component.
+ *
+ * @author TCSASSEMBLER
+ * @version 1.0
+ */
+public enum CompetitionType {
+	STUDIO,
+	SOFTWARE
+}
