d2638825c26ce10e079b655e9631246f21048edf
mashannon168 <mashannon168@fb370eea-3af6-4597-97f7-f7400a59c12a>
2010-10-07 14:39:53 +0000

    created a branch as bugr_r3
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@190226 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ project_service/branches/bugr_r3/src/java/main/com/topcoder/service/project/ProjectData.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2008 TopCoder Inc., All Rights Reserved.
+ */
+package com.topcoder.service.project;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityResult;
+import javax.persistence.FieldResult;
+import javax.persistence.Id;
+import javax.persistence.SqlResultSetMapping;
+
+/**
+ * <p>
+ * This data object serves to communicate between project data between the bean and its client. It also serves as a base
+ * class for the Project class.
+ * </p>
+ * <p>
+ * We simply define three properties and getter/setters for these properties. Note that this is a dumb DTO - no
+ * validation is done.
+ * </p>
+ * <p>
+ * <b>Thread Safety</b>: This class is not thread safe as it has mutable state.
+ * </p>
+ *
+ * @author humblefool, FireIce
+ * @version 1.0
+ */
+@SqlResultSetMapping(
+		name="GetProjectsResult",
+		entities={@EntityResult(entityClass=ProjectData.class,
+				fields={@FieldResult(name="projectId",      column="project_id"),
+			            @FieldResult(name="name", column="name"),
+			            @FieldResult(name="description",           column="description")}
+		)})
+@Entity
+public class ProjectData implements Serializable {
+    /**
+     * <p>
+     * Represents the serial version unique id.
+     * </p>
+     */
+    private static final long serialVersionUID = 9053041638081689882L;
+
+    /**
+     * <p>
+     * Represents the ID of this project.
+     * </p>
+     * <p>
+     * It uses <code>Long</code> type instead of <code>long</code> type to allow for null values to be set before
+     * entity creation in persistence. This variable is mutable and is retrieved by the {@link #getProjectId()} method
+     * and set by the {@link #setProjectId(Long)} method. It is initialized to null, and may be set to ANY value.
+     * </p>
+     */
+	@Id
+    private Long projectId;
+
+    /**
+     * <p>
+     * Represents the name of this project.
+     * </p>
+     * <p>
+     * This variable is mutable and is retrieved by the {@link #getName()} method and set by the
+     * {@link #setName(String)} method. It is initialized to null, and may be set to ANY value, including null/empty
+     * string.
+     * </p>
+     */
+    private String name;
+
+    /**
+     * <p>
+     * Represents the description of this project.
+     * </p>
+     * <p>
+     * This variable is mutable and is retrieved by the getDescription method and set by the setDescription method. It
+     * is initialized to null, and may be set to ANY value, including null/empty string.
+     * </p>
+     */
+    private String description;
+
+
+
+    /**
+     * <p>
+     * Constructs a <code>ProjectData</code> instance.
+     * </p>
+     */
+    public ProjectData() {
+    }
+
+    /**
+     * <p>
+     * Gets the ID of the project.
+     * </p>
+     *
+     * @return The ID of the project.
+     */
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    /**
+     * <p>
+     * Sets the ID of this project.
+     * </p>
+     *
+     * @param projectId
+     *            The desired ID of this project. ANY value.
+     */
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    /**
+     * <p>
+     * Gets the name of the project.
+     * </p>
+     *
+     * @return The name of the project.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * <p>
+     * Sets the name of this project.
+     * </p>
+     *
+     * @param name
+     *            The desired name of this project. ANY value.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * <p>
+     * Gets the description of the project.
+     * </p>
+     *
+     * @return The description of the project.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * <p>
+     * Sets the description of this project.
+     * </p>
+     *
+     * @param description
+     *            The desired description of this project. ANY value.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
