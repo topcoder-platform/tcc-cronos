071a86df945709833b26c49fe591057e53549eca
mashannon168 <mashannon168@fb370eea-3af6-4597-97f7-f7400a59c12a>
2010-10-27 20:18:05 +0000

    created a branch as studio_declaration
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@192001 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ cloud_vm_service/branches/studio_declaration/src/java/main/com/topcoder/direct/services/view/dto/cloudvm/VMInstanceType.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2010 TopCoder Inc., All Rights Reserved.
+ */
+package com.topcoder.direct.services.view.dto.cloudvm;
+
+/**
+ * This class represents the VM instance type.
+ *
+ * Thread-safety: Mutable and not thread-safe.
+ *
+ * @author Standlove, TCSDEVELOPER
+ * @version 1.0
+ */
+public class VMInstanceType extends AbstractIdEntity {
+    /**
+     * The serial version uid.
+     */
+    private static final long serialVersionUID = 4320570143750879L;
+
+    /**
+     * Represents the instance type name. It has getter & setter. It can be any value.
+     */
+    private String name;
+
+    /**
+     * Empty constructor.
+     */
+    public VMInstanceType() {
+    }
+
+    /**
+     * Getter for the namesake instance variable. Simply return the namesake instance variable.
+     *
+     * @return field value
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter for the namesake instance variable. Simply set the value to the namesake instance variable.
+     *
+     * @param name value to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
