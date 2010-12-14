e6794bb91535af7a4d622af38b33f94306cbeb1e
jueyey <jueyey@fb370eea-3af6-4597-97f7-f7400a59c12a>
2010-12-14 14:50:14 +0000

    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@195123 fb370eea-3af6-4597-97f7-f7400a59c12a

--- late_deliverables_management/tags/version-1.0.0.1/test_files/DBDrop_tcs.sql
+++
@@ -1,7 +0,0 @@
-database tcs_catalog;
-
-DROP TABLE 'informix'.late_deliverable;
-DROP TABLE 'informix'.project_phase;
-DROP TABLE 'informix'.project;
-DROP TABLE 'informix'.resource_info;
-DROP TABLE 'informix'.resource;
--- late_deliverables_management/tags/version-1.0.0.1/test_files/stress/clear.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM tcs_catalog:late_deliverable;
-DELETE FROM tcs_catalog:project_phase;
-DELETE FROM tcs_catalog:project;
-DELETE FROM tcs_catalog:resource_info;
-DELETE FROM tcs_catalog:resource;
-DELETE FROM corporate_oltp:tc_direct_project;
-DELETE FROM corporate_oltp:user_permission_grant;
