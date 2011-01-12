8eed68cd267023596871c0cd3a5520d9a5901775
tingyifang <tingyifang@fb370eea-3af6-4597-97f7-f7400a59c12a>
2011-01-12 20:22:10 +0000

    rebuild 1.0.2 successfully
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@196518 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ late_deliverables_management/tags/version-1.0.2.1/test_files/stress/clear.sql
@@ -0,0 +1,7 @@
+DELETE FROM tcs_catalog:late_deliverable;
+DELETE FROM tcs_catalog:project_phase;
+DELETE FROM tcs_catalog:project;
+DELETE FROM tcs_catalog:resource_info;
+DELETE FROM tcs_catalog:resource;
+DELETE FROM corporate_oltp:tc_direct_project;
+DELETE FROM corporate_oltp:user_permission_grant;
