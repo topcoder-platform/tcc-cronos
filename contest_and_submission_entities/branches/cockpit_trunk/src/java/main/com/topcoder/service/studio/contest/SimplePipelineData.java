5de37b2a9a172059636ceec6a93bb8453b536674
mashannon168 <mashannon168@fb370eea-3af6-4597-97f7-f7400a59c12a>
2011-02-08 17:33:53 +0000

    Pipeline member cost calculation update and query cleanup.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@197881 fb370eea-3af6-4597-97f7-f7400a59c12a

--- contest_and_submission_entities/branches/cockpit_trunk/src/java/main/com/topcoder/service/studio/contest/SimplePipelineData.java
+++ contest_and_submission_entities/branches/cockpit_trunk/src/java/main/com/topcoder/service/studio/contest/SimplePipelineData.java
@@ -31,7 +31,6 @@ import javax.persistence.Transient;
  * @since Cockpit Pipeline Release Assembly 1 v1.0 
  */
 @SqlResultSetMapping(name = "SimplePipelineDataResults", entities = { @EntityResult(entityClass = SimplePipelineData.class, fields = {
-        @FieldResult(name = "pipelineInfoId", column = "id"),
         @FieldResult(name = "contestId", column = "contest_id"),
         @FieldResult(name = "projectId", column = "project_id"), 
         @FieldResult(name = "pname", column = "pname"),
@@ -51,30 +50,11 @@ import javax.persistence.Transient;
         @FieldResult(name = "createTime", column = "creation_time"),
         @FieldResult(name = "modifyTime", column = "modification_time"),
         
-        @FieldResult(name = "clientName", column = "client_name"),
-        
-        @FieldResult(name = "reviewPayment", column = "review_payment"),
-        @FieldResult(name = "specReviewPayment", column = "specification_review_payment"),
+        @FieldResult(name = "clientName", column = "client_name"),    
         @FieldResult(name = "totalPrize", column = "tot_prize"),
-        @FieldResult(name = "dr", column = "dr"),
         @FieldResult(name = "contestFee", column = "contest_fee"),
-/*        
-        @FieldResult(name = "shortDesc", column = "short_desc"),
-        @FieldResult(name = "longDesc", column = "long_desc"),
-        @FieldResult(name = "eligibility", column = "eligibility"),
-*/        
+            
         @FieldResult(name = "manager", column = "manager"),
-        @FieldResult(name = "reviewer", column = "reviewer"),
-        @FieldResult(name = "architect", column = "architect"),
-        @FieldResult(name = "salesPerson", column = "sales_person"),
-        
-        @FieldResult(name = "clientApproval", column = "client_approval"),
-        @FieldResult(name = "pricingApproval", column = "pricing_approval"),
-        @FieldResult(name = "hasWikiSpecification", column = "has_wiki_specification"),
-        @FieldResult(name = "passedSpecReview", column = "passed_spec_review"),
-        @FieldResult(name = "hasDependentCompetitions", column = "has_dependent_competitions"),
-        @FieldResult(name = "wasReposted", column = "was_reposted"),
-        @FieldResult(name = "notes", column = "notes"),
         
         @FieldResult(name = "cperm", column = "cperm"),
         @FieldResult(name = "pperm", column = "pperm"),
@@ -88,9 +68,6 @@ public class SimplePipelineData {
 
 	/** Represents the client project name. */
     private String cpname;
-	
-    /** Represents the pipeline info id. */
-    private Long pipelineInfoId;
 
     @Id
     /** Represents the contest id. */
@@ -139,62 +116,14 @@ public class SimplePipelineData {
     /** The client name. */
     private String clientName;
 
-    /** The review payment. */
-    private Double reviewPayment;
-
-    /** The spec review payment. */
-    private Double specReviewPayment;
-
     /** The total prize. */
     private Double totalPrize;
-
-    /** The dr. */
-    private Double dr;
 
     /** The contest fee. */
     private Double contestFee;
 
-    /** The short desc. */
-//    private String shortDesc;
-
-    /** The long desc. */
-//    private String longDesc;
-
-    /** The eligibility. */
-//    private String eligibility;
-
     /** The manager. */
     private String manager;
-
-    /** The reviewer. */
-    private String reviewer;
-
-    /** The architect. */
-    private String architect;
-
-    /** The sales person. */
-    private String salesPerson;
-
-    /** The client approval. */
-    private Boolean clientApproval;
-
-    /** The pricing approval. */
-    private Boolean pricingApproval;
-
-    /** The has wiki specification. */
-    private Boolean hasWikiSpecification;
-
-    /** The passed spec review. */
-    private Boolean passedSpecReview;
-
-    /** The has dependent competitions. */
-    private Boolean hasDependentCompetitions;
-
-    /** The was reposted. */
-    private Boolean wasReposted;
-
-    /** The notes. */
-    private String notes;
 
     /** Represents the permission for contest. */
     private String cperm;
@@ -341,25 +270,6 @@ public class SimplePipelineData {
      */
     public void setContestType(String contestType) {
         this.contestType = contestType;
-    }
-
-    /**
-     * Gets the pipeline info id.
-     * 
-     * @return the pipeline info id
-     */
-    public Long getPipelineInfoId() {
-        return this.pipelineInfoId;
-    }
-
-    /**
-     * Sets the pipeline info id.
-     * 
-     * @param pipelineInfoId
-     *            the new pipeline info id
-     */
-    public void setPipelineInfoId(Long pipelineInfoId) {
-        this.pipelineInfoId = pipelineInfoId;
     }
 
     /**
@@ -515,44 +425,6 @@ public class SimplePipelineData {
     }
 
     /**
-     * Gets the review payment.
-     * 
-     * @return the review payment
-     */
-    public Double getReviewPayment() {
-        return this.reviewPayment;
-    }
-
-    /**
-     * Sets the review payment.
-     * 
-     * @param reviewPayment
-     *            the new review payment
-     */
-    public void setReviewPayment(Double reviewPayment) {
-        this.reviewPayment = reviewPayment;
-    }
-
-    /**
-     * Gets the spec review payment.
-     * 
-     * @return the spec review payment
-     */
-    public Double getSpecReviewPayment() {
-        return this.specReviewPayment;
-    }
-
-    /**
-     * Sets the spec review payment.
-     * 
-     * @param specReviewPayment
-     *            the new spec review payment
-     */
-    public void setSpecReviewPayment(Double specReviewPayment) {
-        this.specReviewPayment = specReviewPayment;
-    }
-
-    /**
      * Gets the total prize.
      * 
      * @return the total prize
@@ -569,25 +441,6 @@ public class SimplePipelineData {
      */
     public void setTotalPrize(Double totalPrize) {
         this.totalPrize = totalPrize;
-    }
-
-    /**
-     * Gets the dr.
-     * 
-     * @return the dr
-     */
-    public Double getDr() {
-        return this.dr;
-    }
-
-    /**
-     * Sets the dr.
-     * 
-     * @param dr
-     *            the new dr
-     */
-    public void setDr(Double dr) {
-        this.dr = dr;
     }
 
     /**
@@ -610,63 +463,6 @@ public class SimplePipelineData {
     }
 
     /**
-     * Gets the short desc.
-     * 
-     * @return the short desc
-     
-    public String getShortDesc() {
-        return this.shortDesc;
-    }*/
-
-    /**
-     * Sets the short desc.
-     * 
-     * @param shortDesc
-     *            the new short desc
-     
-    public void setShortDesc(String shortDesc) {
-        this.shortDesc = shortDesc;
-    }*/
-
-    /**
-     * Gets the long desc.
-     * 
-     * @return the long desc
-     
-    public String getLongDesc() {
-        return this.longDesc;
-    }*/
-
-    /**
-     * Sets the long desc.
-     * 
-     * @param longDesc
-     *            the new long desc
-     
-    public void setLongDesc(String longDesc) {
-        this.longDesc = longDesc;
-    }*/
-
-    /**
-     * Gets the eligibility.
-     * 
-     * @return the eligibility
-     
-    public String getEligibility() {
-        return this.eligibility;
-    }*/
-
-    /**
-     * Sets the eligibility.
-     * 
-     * @param eligibility
-     *            the new eligibility
-    
-    public void setEligibility(String eligibility) {
-        this.eligibility = eligibility;
-    } */
-
-    /**
      * Gets the manager.
      * 
      * @return the manager
@@ -685,195 +481,7 @@ public class SimplePipelineData {
         this.manager = manager;
     }
 
-    /**
-     * Gets the reviewer.
-     * 
-     * @return the reviewer
-     */
-    public String getReviewer() {
-        return this.reviewer;
-    }
 
-    /**
-     * Sets the reviewer.
-     * 
-     * @param reviewer
-     *            the new reviewer
-     */
-    public void setReviewer(String reviewer) {
-        this.reviewer = reviewer;
-    }
-
-    /**
-     * Gets the architect.
-     * 
-     * @return the architect
-     */
-    public String getArchitect() {
-        return this.architect;
-    }
-
-    /**
-     * Sets the architect.
-     * 
-     * @param architect
-     *            the new architect
-     */
-    public void setArchitect(String architect) {
-        this.architect = architect;
-    }
-
-    /**
-     * Gets the sales person.
-     * 
-     * @return the sales person
-     */
-    public String getSalesPerson() {
-        return this.salesPerson;
-    }
-
-    /**
-     * Sets the sales person.
-     * 
-     * @param salesPerson
-     *            the new sales person
-     */
-    public void setSalesPerson(String salesPerson) {
-        this.salesPerson = salesPerson;
-    }
-
-    /**
-     * Gets the client approval.
-     * 
-     * @return the client approval
-     */
-    public Boolean getClientApproval() {
-        return this.clientApproval;
-    }
-
-    /**
-     * Sets the client approval.
-     * 
-     * @param clientApproval
-     *            the new client approval
-     */
-    public void setClientApproval(Boolean clientApproval) {
-        this.clientApproval = clientApproval;
-    }
-
-    /**
-     * Gets the pricing approval.
-     * 
-     * @return the pricing approval
-     */
-    public Boolean getPricingApproval() {
-        return this.pricingApproval;
-    }
-
-    /**
-     * Sets the pricing approval.
-     * 
-     * @param pricingApproval
-     *            the new pricing approval
-     */
-    public void setPricingApproval(Boolean pricingApproval) {
-        this.pricingApproval = pricingApproval;
-    }
-
-    /**
-     * Gets the checks for wiki specification.
-     * 
-     * @return the checks for wiki specification
-     */
-    public Boolean getHasWikiSpecification() {
-        return this.hasWikiSpecification;
-    }
-
-    /**
-     * Sets the checks for wiki specification.
-     * 
-     * @param hasWikiSpecification
-     *            the new checks for wiki specification
-     */
-    public void setHasWikiSpecification(Boolean hasWikiSpecification) {
-        this.hasWikiSpecification = hasWikiSpecification;
-    }
-
-    /**
-     * Gets the passed spec review.
-     * 
-     * @return the passed spec review
-     */
-    public Boolean getPassedSpecReview() {
-        return this.passedSpecReview;
-    }
-
-    /**
-     * Sets the passed spec review.
-     * 
-     * @param passedSpecReview
-     *            the new passed spec review
-     */
-    public void setPassedSpecReview(Boolean passedSpecReview) {
-        this.passedSpecReview = passedSpecReview;
-    }
-
-    /**
-     * Gets the checks for dependent competitions.
-     * 
-     * @return the checks for dependent competitions
-     */
-    public Boolean getHasDependentCompetitions() {
-        return this.hasDependentCompetitions;
-    }
-
-    /**
-     * Sets the checks for dependent competitions.
-     * 
-     * @param hasDependentCompetitions
-     *            the new checks for dependent competitions
-     */
-    public void setHasDependentCompetitions(Boolean hasDependentCompetitions) {
-        this.hasDependentCompetitions = hasDependentCompetitions;
-    }
-
-    /**
-     * Gets the was reposted.
-     * 
-     * @return the was reposted
-     */
-    public Boolean getWasReposted() {
-        return this.wasReposted;
-    }
-
-    /**
-     * Sets the was reposted.
-     * 
-     * @param wasReposted
-     *            the new was reposted
-     */
-    public void setWasReposted(Boolean wasReposted) {
-        this.wasReposted = wasReposted;
-    }
-
-    /**
-     * Gets the notes.
-     * 
-     * @return the notes
-     */
-    public String getNotes() {
-        return this.notes;
-    }
-
-    /**
-     * Sets the notes.
-     * 
-     * @param notes
-     *            the new notes
-     */
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
 
     /**
      * Gets the cperm.
