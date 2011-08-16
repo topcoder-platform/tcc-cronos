44e3b0e551fda224428f488a298cbe497cb1fc7c
mashannon168 <mashannon168@fb370eea-3af6-4597-97f7-f7400a59c12a>
2011-08-16 13:29:09 +0000

    created a branch as before_replatforming
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@206083 fb370eea-3af6-4597-97f7-f7400a59c12a

---
+++ cloud_vm_service/branches/before_replatforming/src/java/main/com/topcoder/direct/services/view/dto/cloudvm/VMAccountUser.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2011 TopCoder Inc., All Rights Reserved.
+ */
+package com.topcoder.direct.services.view.dto.cloudvm;
+
+/**
+ * This class represents the VM account user for the Amazon EC2. It contains the user id.
+ *
+ * Thread-safety: Mutable and not thread-safe.
+ *
+ * @author TCSASSEMBLER
+ * @version 1.0
+ */
+public class VMAccountUser extends AbstractIdEntity {
+    /**
+     * The serial version uid.
+     */
+    private static final long serialVersionUID = 2742320281043253L;
+
+    /**
+     * Represents the VM account id.
+     */
+    private long vmAccountId;
+
+    /**
+     * User id.
+     */
+    private long userId;
+
+    /**
+     * Empty constructor.
+     */
+    public VMAccountUser() {
+    }
+
+    /**
+     * Gets the VM Account id.
+     * @return  the VM Account id.
+     */
+    public long getVmAccountId() {
+        return vmAccountId;
+    }
+
+    /**
+     * Sets the VM Account id.
+     * @param vmAccountId  the VM Account id.
+     */
+    public void setVmAccountId(long vmAccountId) {
+        this.vmAccountId = vmAccountId;
+    }
+
+
+    /**
+     * Getter for the namesake instance variable. Simply return the namesake instance variable.
+     *
+     * @return the user id.
+     */
+    public long getUserId() {
+        return userId;
+    }
+
+    /**
+     * Setter for the namesake instance variable. Simply set the value to the namesake instance variable.
+     *
+     * @param userId the user id.
+     */
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+}
+
---
+++ cloud_vm_service/branches/before_replatforming/src/java/main/com/topcoder/direct/services/view/dto/cloudvm/VMInstanceData.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright (C) 2010 TopCoder Inc., All Rights Reserved.
+ */
+package com.topcoder.direct.services.view.dto.cloudvm;
+
+import java.io.Serializable;
+
+/**
+ * This class represents the VM instance data. It contains the vm instance, status, and VM Manager's handle.
+ *
+ * Thread-safety: Mutable and not thread-safe.
+ *
+ * @author Standlove, TCSDEVELOPER
+ * @version 1.0
+ */
+public class VMInstanceData implements Serializable {
+    /**
+     * Represents the vm instance status. It has getter & setter. It can be any value.
+     */
+    private VMInstanceStatus status;
+
+    /**
+     * Represents the VM instance. It has getter & setter. It can be any value.
+     */
+    private VMInstance instance;
+
+    /**
+     * Represents the VM manager's handle. It has getter & setter. It can be any value.
+     */
+    private String managerHandle;
+    
+    /**
+     * Stores contest name for readability.
+     * @since BUGR-3232
+     */
+    private String contestName;
+    
+    /**
+     * Stores tc image name for readability.
+     * @since BUGR-3232
+     */
+    private String vmImageTcName;
+
+    /**
+     * Stores account name for readability.
+     */
+    private String accountName;
+
+    /**
+     * Stores vm creation time for readability.
+     */
+    private String vmCreationTime;
+    
+    /**
+     * vm usage for readability.
+     */         
+    private String usage;
+    
+    /**
+     * Empty constructor.
+     */
+    public VMInstanceData() {
+    }
+
+    /**
+     * Getter for the namesake instance variable. Simply return the namesake instance variable.
+     *
+     * @return field value
+     */
+    public VMInstanceStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * Setter for the namesake instance variable. Simply set the value to the namesake instance variable.
+     *
+     * @param status value to set
+     */
+    public void setStatus(VMInstanceStatus status) {
+        this.status = status;
+    }
+
+    /**
+     * Getter for the namesake instance variable. Simply return the namesake instance variable.
+     *
+     * @return field value
+     */
+    public VMInstance getInstance() {
+        return instance;
+    }
+
+    /**
+     * Setter for the namesake instance variable. Simply set the value to the namesake instance variable.
+     *
+     * @param instance value to set
+     */
+    public void setInstance(VMInstance instance) {
+        this.instance = instance;
+    }
+
+    /**
+     * Getter for the namesake instance variable. Simply return the namesake instance variable.
+     *
+     * @return field value
+     */
+    public String getManagerHandle() {
+        return managerHandle;
+    }
+
+    /**
+     * Setter for the namesake instance variable. Simply set the value to the namesake instance variable.
+     *
+     * @param managerHandle value to set
+     */
+    public void setManagerHandle(String managerHandle) {
+        this.managerHandle = managerHandle;
+    }
+   
+    /**
+     * Getter for the namesake instance variable. Simply return the namesake instance variable.
+     *
+     * @return field value
+     * @since BUGR-3932
+     */
+    public String getContestName() {
+        return contestName;
+    }
+    
+    /**
+     * Setter for the namesake instance variable. Simply set the value to the namesake instance variable.
+     *
+     * @param managerHandle value to set
+     * @since BUGR-3932
+     */
+    public void setContestName(String contestName) {
+        this.contestName = contestName;
+    }
+    
+    /**
+     * Getter for the namesake instance variable. Simply return the namesake instance variable.
+     *
+     * @return field value
+     * @since BUGR-3932
+     */
+    public String getVmImageTcName() {
+        return vmImageTcName;
+    }
+
+    /**
+     * Setter for the namesake instance variable. Simply set the value to the namesake instance variable.
+     *
+     * @param managerHandle value to set
+     * @since BUGR-3932
+     */
+    public void setVmImageTcName(String vmImageTcName) {
+        this.vmImageTcName = vmImageTcName;
+    }
+
+    /**
+     * Getter for the namesake instance variable. Simply return the namesake instance variable.
+     *
+     * @return field value
+     */
+    public String getAccountName() {
+        return accountName;
+    }
+
+    /**
+     * Setter for the namesake instance variable. Simply set the value to the namesake instance variable.
+     *
+     * @param accountName value to set
+     */
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    /**
+     * Getter for the namesake instance variable. Simply return the namesake instance variable.
+     *
+     * @return field value
+     */
+    public String getVmCreationTime() {
+        return vmCreationTime;
+    }
+
+    /**
+     * Setter for the namesake instance variable. Simply set the value to the namesake instance variable.
+     *
+     * @param vmCreationTime value to set
+     */
+    public void setVmCreationTime(String vmCreationTime) {
+        this.vmCreationTime = vmCreationTime;
+    }
+    
+    
+    /**
+     * Getter for the namesake instance variable. Simply return the namesake instance variable.
+     *
+     * @return field value
+     */
+    public String getUsage() {
+        return usage;
+    }
+
+    /**
+     * Setter for the namesake instance variable. Simply set the value to the namesake instance variable.
+     *
+     * @param usage value to set
+     */
+    public void setUsage(String usage) {
+        this.usage = usage;
+    }
+    
+}
+
