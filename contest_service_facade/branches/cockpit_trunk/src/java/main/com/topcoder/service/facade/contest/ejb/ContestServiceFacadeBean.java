5a1170ab3586f67876c883a364ecccb739874a5e
mashannon168 <mashannon168@fb370eea-3af6-4597-97f7-f7400a59c12a>
2011-09-14 21:59:31 +0000

    studio missed review/screening cost in email.
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@207483 fb370eea-3af6-4597-97f7-f7400a59c12a

--- contest_service_facade/branches/cockpit_trunk/src/java/main/com/topcoder/service/facade/contest/ejb/ContestServiceFacadeBean.java
+++ contest_service_facade/branches/cockpit_trunk/src/java/main/com/topcoder/service/facade/contest/ejb/ContestServiceFacadeBean.java
@@ -3150,7 +3150,7 @@ public class ContestServiceFacadeBean implements ContestServiceFacadeLocal, Cont
             } else {
                 // studio competition
                 totalFee = Double.parseDouble((String) contest.getProperty(ProjectPropertyType.ADMIN_FEE_PROJECT_PROPERTY_KEY))
-                    + Double.parseDouble((String) contest.getProperty(ProjectPropertyType.SPEC_REVIEW_COSTS_PROJECT_PROPERTY_KEY));
+                    + Double.parseDouble((String) contest.getProperty(ProjectPropertyType.SPEC_REVIEW_COSTS_PROJECT_PROPERTY_KEY))
                     + Double.parseDouble((String) contest.getProperty(ProjectPropertyType.REVIEW_COSTS_PROJECT_PROPERTY_KEY));
                 for (Prize prize : competition.getProjectHeader().getPrizes()) {
                     totalFee = totalFee + prize.getPrizeAmount() * prize.getNumberOfSubmissions();
