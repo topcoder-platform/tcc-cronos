f76721d46853fcbe4b7c33a089d5aacead2fed62
gjw99 <gjw99@fb370eea-3af6-4597-97f7-f7400a59c12a>
2011-11-28 00:37:02 +0000

    update to 1.1
    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@210486 fb370eea-3af6-4597-97f7-f7400a59c12a

--- terms_of_use/trunk/test_files/DBDrop.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE 'informix'.id_sequences;
-
-DROP TABLE 'informix'.project_role_terms_of_use_xref;
-DROP TABLE 'informix'.user_terms_of_use_ban_xref;
-DROP TABLE 'informix'.user_terms_of_use_xref;
-DROP TABLE 'informix'.terms_of_use_type;
-DROP TABLE 'informix'.terms_of_use;
-DROP TABLE 'informix'.user;
-DROP TABLE 'informix'.timezone_lu;
