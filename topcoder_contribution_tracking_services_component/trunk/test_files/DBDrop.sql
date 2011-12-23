32136b32bda837daa41f85427c62478219a11abb
arylio <arylio@fb370eea-3af6-4597-97f7-f7400a59c12a>
2011-12-23 02:43:51 +0000

    git-svn-id: https://coder.topcoder.com/tcs/clients/cronos/components@211356 fb370eea-3af6-4597-97f7-f7400a59c12a

--- topcoder_contribution_tracking_services_component/trunk/test_files/DBDrop.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE member_contribution_points;
-DROP TABLE project_contest_cp_config;
-DROP TABLE direct_project_cp_config;
-DROP TABLE project;
-DROP TABLE tc_direct_project;
--- topcoder_contribution_tracking_services_component/trunk/test_files/accuracy/DBClear.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM project_contest_cp_config;
-DELETE FROM direct_project_cp_config;
-DELETE FROM member_contribution_points;
-DELETE FROM project;
-DELETE FROM tc_direct_project;
